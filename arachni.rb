bf4327cab08d82f6ff07562aab7ee31079aa89f7
Tasos Laskos <tasos.laskos@gmail.com>
2010-06-26 20:02:05 +0000

    updated help message on invalid urls

--- arachni.rb
+++ arachni.rb
@@ -250,7 +250,7 @@ rescue
   puts "Error: Invalid URL argument."
   puts "URL must be of type 'scheme://username:password@subdomain." +
          "domain.tld:port/path?query_string#anchor'"
-  puts "If there is no subdomain do *not* ommit the \"www\"."
+  puts "Be careful with the \"www\"."
   puts
   puts "Examples:"
   puts "    http://www.google.com"
