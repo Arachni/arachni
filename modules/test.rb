e2841627b81407eaed6d58a4a24c1f6580160e40
Tasos Laskos <tasos.laskos@gmail.com>
2010-06-29 19:05:29 +0000

    added super() call in constructor

--- modules/test.rb
+++ modules/test.rb
@@ -19,21 +19,23 @@ class Test < Arachni::Module
   include Arachni::ModuleRegistrar
     
   def initialize( page_data, structure )
+    super( page_data, structure )
+    
     @page_data = page_data
   end
 
   def prepare( )
-    puts 'In Test.prepare()'
+    puts '-------- In Test.prepare()'
   end
   
   
   def run( )
-    puts 'In Test.run()'
+    puts '-------- In Test.run()'
   end
 
 
   def clean_up( )
-    puts 'In Test.clean_up()'
+    puts '-------- In Test.clean_up()'
   end
   
   
--- modules/test2.rb
+++ modules/test2.rb
@@ -19,21 +19,23 @@ class Test2 < Arachni::Module
   include Arachni::ModuleRegistrar
     
   def initialize( page_data, structure )
+    super( page_data, structure )
+    
     @page_data = page_data
   end
 
   def prepare( )
-    puts 'In Test2.prepare()'
+    puts '-------- In Test2.prepare()'
   end
   
   
   def run( )
-    puts 'In Test2.run()'
+    puts '-------- In Test2.run()'
   end
 
 
   def clean_up( )
-    puts 'In Test2.clean_up()'
+    puts '-------- In Test2.clean_up()'
   end
   
   
