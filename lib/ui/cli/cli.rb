3ffd5c70def66f1f361b906027bcccf8d5f80600
Tasos Laskos <tasos.laskos@gmail.com>
2010-07-20 20:26:24 +0000

    parse_opts(): updated exception handling

--- lib/ui/cli/cli.rb
+++ lib/ui/cli/cli.rb
@@ -128,6 +128,12 @@ class CLI
                 when 'mods'
                     begin
                         @arachni.mod_load( @opts[:mods] )
+                    rescue Arachni::Exceptions::ModNotFound => e
+                        print_error( e.to_s )
+                        print_info( "Run arachni with the '-l' parameter" +
+                            " to see all available modules." )
+                        print_line
+                        exit 0
                     rescue Arachni::Exceptions::DepModNotFound => e
                         print_error( e.to_s )
                         print_line
