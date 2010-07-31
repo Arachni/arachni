590daf2d3e4f5a7ee6bde564bc9f0aaf4a46942d
Tasos Laskos <tasos.laskos@gmail.com>
2010-07-31 08:19:17 +0000

    added get_response( ): upped the retry counter to 7 and generalized exception handling

--- lib/anemone/http.rb
+++ lib/anemone/http.rb
@@ -13,8 +13,9 @@ require 'rubygems'
 require 'anemone'
 
 #
-# Overides Anemone's HTTP class method refresh_connection( url )
-# adding proxy support
+# Overides Anemone's HTTP class methods:
+#  o refresh_connection( ): added proxy support
+#  o get_response( ): upped the retry counter to 7 and generalized exception handling
 #
 # @author: Anastasios "Zapotek" Laskos
 #                                      <tasos.laskos@gmail.com>
@@ -22,6 +23,8 @@ require 'anemone'
 # @version: 0.1-pre
 #
 class Anemone::HTTP
+
+    include Arachni::UI::Output
 
     def refresh_connection( url )
 
@@ -35,6 +38,38 @@ class Anemone::HTTP
         end
 
         @connections[url.host][url.port] = http.start
+    end
+
+    #
+    # Get an HTTPResponse for *url*, sending the appropriate User-Agent string
+    #
+    def get_response(url, referer = nil)
+        full_path = url.query.nil? ? url.path : "#{url.path}?#{url.query}"
+
+        opts = {}
+        opts['User-Agent'] = user_agent if user_agent
+        opts['Referer'] = referer.to_s if referer
+        opts['Cookie'] = @cookie_store.to_s unless @cookie_store.empty? || (!accept_cookies? && @opts[:cookies].nil?)
+
+        retries = 0
+        begin
+            start = Time.now()
+            response = connection(url).get(full_path, opts)
+            finish = Time.now()
+            response_time = ((finish - start) * 1000).round
+            @cookie_store.merge!(response['Set-Cookie']) if accept_cookies?
+            return response, response_time
+        rescue Exception => e
+            retries += 1
+            
+            print_error( e.to_s )
+            print_info( ( 7 - retries ).to_s +
+                ' retries remaining for url: ' + url.to_s )
+                
+            print_debug_backtrace( e )
+            refresh_connection(url)
+            retry unless retries > 7
+        end
     end
 
 end
