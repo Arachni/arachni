f1c4a12330a7d082c2b71d5efc3393205ec59c53
Tasos Laskos <tasos.laskos@gmail.com>
2010-08-01 01:04:50 +0000

    updated svn:keywords

--- lib/page.rb
+++ lib/page.rb
@@ -24,18 +24,50 @@ module Arachni
 #
 class Page
 
+    #
+    # @return    [String]    url of the page
+    #
     attr_accessor :url
+    
+    #
+    # @return    [Hash]    url variables
+    #
     attr_accessor :query_vars
     
+    #
+    # @return    [String]    the HTML response
+    #
     attr_accessor :html
     
+    #
+    # @return    [Hash]    response headers
+    #
     attr_accessor :headers
     
+    #
+    # @see Analyzer#get_headers
+    #
+    # @return    [Hash]    auditable HTTP request headers
+    #
+    attr_accessor :request_headers
+    
+    #
+    # @see Analyzer#run
+    # @see Analyzer#get_links
+    # @see Analyzer#get_forms
+    # @see Analyzer#get_cookies
+    #
+    # @return    [Hash]    auditable HTML elements (links/forms/cookies)
+    #
     attr_accessor :elements
     
+    #
+    # Cookies extracted from the supplied cookiejar
+    #
+    # @return    [Hash]
+    #
     attr_accessor :cookiejar
     
-        
     def initialize( opts = {} )
         opts.each {
             |k, v|
@@ -44,14 +76,23 @@ class Page
 
     end
     
+    #
+    # Returns the form elements in {Page#elements}
+    #
     def get_forms
         elements['forms']
     end
     
+    #
+    # Returns the links elements in {Page#elements}
+    #
     def get_links
         elements['links']
     end
     
+    #
+    # Returns the cookies elements in {Page#elements}
+    #
     def get_cookies
         elements['cookies']
     end
