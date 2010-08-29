3e12fe38a02ba7c8696fe053b5b58afb82559a6f
Tasos Laskos <tasos.laskos@gmail.com>
2010-08-30 00:22:58 +0300

    moved the __get_path() method that a lot of the modules share into a Utililities module

---
+++ lib/module/utilities.rb
@@ -0,0 +1,60 @@
+=begin
+  $Id$
+
+                  Arachni
+  Copyright (c) 2010 Anastasios Laskos <tasos.laskos@gmail.com>
+
+  This is free software; you can copy and distribute and modify
+  this program under the term of the GPL v2.0 License
+  (See LICENSE file for details)
+
+=end
+
+module Arachni
+module Module
+
+#
+# Utilities class
+#
+# Includes some useful methods for the system, the modules etc...
+#
+# @author: Anastasios "Zapotek" Laskos
+#                                      <tasos.laskos@gmail.com>
+#                                      <zapotek@segfault.gr>
+# @version: 0.1-pre
+#
+module Utilities
+  
+    #
+    # Gets path from URL
+    #
+    # @param   [String]   url
+    #
+    # @return  [String]   path
+    #
+    def Utilities.get_path( url )
+      
+        splits = []
+        tmp = ''
+        
+        url.each_char {
+            |c|
+            if( c != '/' )
+                tmp += c
+            else
+                splits << tmp
+                tmp = ''
+            end
+        }
+        
+        if( !tmp =~ /\./ )
+          splits << tmp
+        end
+        
+        return splits.join( "/" ) + '/'
+    end
+  
+end  
+
+end
+end
