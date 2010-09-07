525cf8d78cd3cb4d633ebfccc3c88184cf9680a0
Tasos Laskos <tasos.laskos@gmail.com>
2010-09-07 05:27:37 +0300

    Utilities: added rdiff() method

--- lib/module/utilities.rb
+++ lib/module/utilities.rb
@@ -19,7 +19,7 @@ module Module
 # @author: Anastasios "Zapotek" Laskos
 #                                      <tasos.laskos@gmail.com>
 #                                      <zapotek@segfault.gr>
-# @version: 0.1
+# @version: 0.1.1
 #
 module Utilities
   
@@ -50,6 +50,51 @@ module Utilities
         end
         
         return splits.join( "/" ) + '/'
+    end
+    
+    #
+    # Gets the reverse diff (strings that have not changed) between 2 strings
+    #
+    #
+    # text1 = <<END
+    # This is the first test.
+    # Not really sure what else to put here...
+    # END
+    # 
+    # text2 = <<END
+    # This is the second test.
+    # Not really sure what else to put here...
+    # Boo-Yah!
+    # END
+    # 
+    # Arachni::Modules::Utilities.rdiff( text1, text2 )
+    #  # => "This is the  test.\nNot really sure what else to put here...\n"
+    #
+    #
+    # @param  [String]  text1
+    # @param  [String]  text2
+    #
+    # @return  [String]
+    #
+    def Utilities.rdiff( text1, text2 )
+        
+        return text1 if text1 == text2
+        
+        # get the words of the first text in an array
+        words1 = text1.split( /\b/ )
+    
+        # get the words of the second text in an array
+        words2 = text2.split( /\b/ )
+    
+        # get all the words that are different between the 2 arrays
+        # math style!
+        changes  = words1 - words2
+        changes << words2 - words1
+        changes.flatten!
+    
+        # get what hasn't changed (the rdiff, so to speak) as a string
+        return ( words1 - changes ).join( '' ) 
+    
     end
   
 end  
