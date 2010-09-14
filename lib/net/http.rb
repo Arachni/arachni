7bb95684d11633e9759aa91bdad08bbc89f0fa73
Tasos Laskos <tasos.laskos@gmail.com>
2010-09-14 08:54:38 +0300

    removed obsolete Net::HTTPResponse method override

--- lib/net/http.rb
+++ lib/net/http.rb
@@ -13,9 +13,6 @@ require 'net/http'
 # Unfortunaetely Anemone doesn't support HTTP authentication<br/>
 # so we need to get to the core and override Net::HTTPHeader.initialize_http_header( )
 #
-# May not be such a bad thing after all since it'll apply<br/>
-# to the whole system but it feels kinda dirty.
-#
 # @author: Anastasios "Zapotek" Laskos
 #                                      <tasos.laskos@gmail.com>
 #                                      <zapotek@segfault.gr>
@@ -38,19 +35,3 @@ def initialize_http_header( initheader )
 end
 
 end
-
-module Net
-  
-    class HTTPResponse
-
-        #
-        # The time it took for the request to complete.<br/>
-        # (To be populated by {Arachni::Module::HTTP} methods).
-        #
-        # I could hack it directly into Net::HTTPResponse
-        # but that shit's already crazy!
-        #
-        attr_accessor :time  
-    end
-end
-
