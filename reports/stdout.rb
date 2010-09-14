72ab7bf62ea57dc99d49d3eb4007bf0f34f5fa11
Tasos Laskos <tasos.laskos@gmail.com>
2010-09-14 05:56:19 +0300

    bumped version

--- reports/stdout.rb
+++ reports/stdout.rb
@@ -21,7 +21,7 @@ module Reports
 # @author: Anastasios "Zapotek" Laskos
 #                                      <tasos.laskos@gmail.com>
 #                                      <zapotek@segfault.gr>
-# @version: 0.1
+# @version: 0.1.1
 #
 class Stdout < Arachni::Report::Base
     
