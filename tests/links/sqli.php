72948273b27044f7e571888200dc10fc2920a6fb
Tasos Laskos <tasos.laskos@gmail.com>
2010-10-05 16:36:57 +0100

    added test case for blind sql injection

---
+++ tests/links/sqli.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * This page is used to test the SQL injection module.
+ *
+ */
+
+echo <<<EOHTML
+    <pre>
+This form is vulnerable to Cross-Site Scripting.
+    </pre>
+
+<a href="{$_SERVER['PHP_SELF']}?id=1">SQL injection</a>
+EOHTML;
+
+if( $_GET['id'] ) {
+    echo "<pre>";
+    mysql_connect( 'localhost', 'root' );
+    mysql_select_db( 'arachni' );
+
+    $SQL['query']   =<<<SQL
+SELECT *
+  FROM content
+ WHERE id='{$_GET['id']}'
+SQL;
+
+    $result = mysql_query( $SQL['query'] );
+
+    while( $row = mysql_fetch_assoc( $result ) ) {
+       $SQL['result'][]  = $row;
+    }
+
+    if( !$SQL['result'] ) {
+        $SQL['result'] = 'Nothing found.';
+    }
+    
+
+    $SQL['error'] = mysql_error();
+    print_r( $SQL['result'] );
+/*    print_r( $SQL['query'] );*/
+/*    print_r( $SQL['error'] );*/
+    echo "</pre>";
+}
+
+?>
