373ff00aca3328bb28bf86cf25810a2a5e7ed35b
Tasos Laskos <tasos.laskos@gmail.com>
2010-10-21 04:38:56 +0100

    added doc comments to attributes

--- reports/metareport/arachni_metareport.rb
+++ reports/metareport/arachni_metareport.rb
@@ -24,25 +24,124 @@
 #
 class ArachniMetareport
 
-
+    #
+    # The IP address of the datastore_bak
+    #
+    # @return [String]
+    #
     attr_accessor :host
+    
+    #
+    # The port number of the associated web site
+    #
+    # @return   [Integer]
+    #
     attr_accessor :port
+    
+    #
+    # The virtual host for this particular web site
+    #
+    # @return   [Integer]
+    #
     attr_accessor :vhost
+    
+    #
+    # Whether or not SSL is in use on this port
+    #
+    # @return   [Bool]
+    #
     attr_accessor :ssl
     
+    #
+    # Path of the vulnerable script
+    #
+    # @return   [String]
+    #
     attr_accessor :path
+    
+    #
+    # Query part of the vulnerable URI
+    #
+    # @return   [Bool]
+    #
     attr_accessor :query
+    
+    #
+    # HTTP method used for the vulnerability
+    #
+    # The MSF currently supports GET/POST/PATH only, although Arachni will also
+    # provide COOKIE and HEADER if that's the case.
+    #
+    # @return   [String]
+    #
     attr_accessor :method
+    
+    #
+    # Parameters used for the vulnerability
+    #
+    # @return   [Hash]
+    #
     attr_accessor :params
+    
+    #
+    # Headers used for the vulnerability
+    #
+    # Contains cookies.
+    #
+    # @return   [Hash]
+    #
     attr_accessor :headers
+    
+    #
+    # The name of the vulnerable field
+    #
+    # @return   [String]
+    #
     attr_accessor :pname
+    
+    #
+    # A string showing proof of the vulnerability
+    #
+    # @return   [String]
+    #
     attr_accessor :proof
+    
+    #
+    # An integer value from 0 to 5 indicating the risk (5 is highest)
+    #
+    # @return   [Integer]
+    #
     attr_accessor :risk
     
+    #
+    # A string indicating the type of vulnerability
+    #
+    # @return   [String]
+    #
     attr_accessor :name
+    
+    #
+    # Description of the vulnerability
+    #
+    # @return   [String]
+    #
     attr_accessor :description
+    
+    #
+    # No idea what this is...
+    #
+    # @return   [String]
+    #
     attr_accessor :category
     
+    #
+    # An arachni_* exploit of the MSF framework that is able to exploit this
+    # type of vulnerability.
+    #
+    # Ex: unix/webapp/arachni_php_eval
+    #
+    # @return   [String]
+    #
     attr_accessor :exploit
     
     #
