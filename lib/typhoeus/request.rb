7726da1c2b1414c5c5621afdca03fad6de6ba493
Tasos Laskos <tasos.laskos@gmail.com>
2010-10-22 15:57:30 +0100

    o added license header o updated formatting

--- lib/typhoeus/request.rb
+++ lib/typhoeus/request.rb
@@ -1,3 +1,12 @@
+=begin
+                  Arachni
+  Copyright (c) 2010 Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
+
+  This is free software; you can copy and distribute and modify
+  this program under the term of the GPL v2.0 License
+  (See LICENSE file for details)
+
+=end
 
 #
 # Override the on_complete methods of Typhoeus adding support
@@ -7,64 +16,70 @@
 # incremental request id numbers.
 #
 module Typhoeus
-  
+
     class Request
-    
+
         attr_accessor :id
-        
+
         alias :old_initialize :initialize
-    
+
         def initialize( url, options = {} )
-          
-          old_initialize( url, options )
-          
-          @on_complete      = []
-          @handled_response = []
-          @multiple_callbacks = false
-          @train              = false
+
+            old_initialize( url, options )
+
+            @on_complete        = []
+            @handled_response   = []
+            @multiple_callbacks = false
+            @train              = false
         end
-    
-        def on_complete(multi = false, &block)
-          # remember user preference for subsequent calls
-          if( multi || @multiple_callbacks )
-            @multiple_callbacks = true
-            @on_complete << block
-          else
-            @on_complete = block
-          end
-        end
-    
-        def on_complete=(multi = false, proc)
-          # remember user preference for subsequent calls
-          if( multi || @multiple_callbacks )
-            @multiple_callbacks = true
-            @on_complete << proc
-          else
-            @on_complete = proc
-          end
-        end
-        
-        def call_handlers
-          if @on_complete.is_a? Array
-            
-            @on_complete.each do |callback|
-              @handled_response << callback.call(response)
+
+        def on_complete( multi = false, &block )
+
+            # remember user preference for subsequent calls
+            if( multi || @multiple_callbacks )
+                @multiple_callbacks = true
+                @on_complete << block
+            else
+                @on_complete = block
             end
-          else
-            @handled_response << @on_complete.call(response)
-          end
-          
+
+        end
+
+        def on_complete=( multi = false, proc )
+
+            # remember user preference for subsequent calls
+            if( multi || @multiple_callbacks )
+                @multiple_callbacks = true
+                @on_complete << proc
+            else
+                @on_complete = proc
+            end
+
+        end
+
+        def call_handlers
+
+            if @on_complete.is_a? Array
+
+                @on_complete.each do |callback|
+                    @handled_response << callback.call(response)
+                end
+
+            else
+                @handled_response << @on_complete.call(response)
+            end
+
           call_after_complete
         end
-        
+
         def train?
-          @train
+            @train
         end
-        
+
         def train!
-          @train = true
+            @train = true
         end
-        
+
     end
-    
+
 end
