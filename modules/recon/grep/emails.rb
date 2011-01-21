275552cb77efa798065760cdbe2984a5100be131
Tasos Laskos <tasos.laskos@gmail.com>
2011-01-21 02:13:50 +0000

    removed duplicates

--- modules/recon/grep/emails.rb
+++ modules/recon/grep/emails.rb
@@ -25,8 +25,14 @@ class EMails < Arachni::Module::Base
     end
 
     def run( )
+        @@_logged ||= Set.new
+
         regexp = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
-        match_and_log( regexp )
+        match_and_log( regexp ){
+            |email|
+            return false if @@_logged.include?( email )
+            @@_logged << email
+        }
     end
 
     def self.info
