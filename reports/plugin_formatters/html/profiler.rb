6ea6df2afa11103af12d554f36e669f77dad4ec4
Tasos Laskos <tasos.laskos@gmail.com>
2011-02-27 07:10:00 +0000

    added the first draft of the profiler plug-in and formatters for it

---
+++ reports/plugin_formatters/html/profiler.rb
@@ -0,0 +1,56 @@
+=begin
+                  Arachni
+  Copyright (c) 2010-2011 Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
+
+  This is free software; you can copy and distribute and modify
+  this program under the term of the GPL v2.0 License
+  (See LICENSE file for details)
+
+=end
+
+require 'json'
+
+module Arachni
+
+module Reports
+
+class HTML
+    module PluginFormatters
+
+        #
+        # HTML formatter for the results of the Profiler plugin
+        #
+        # @author: Tasos "Zapotek" Laskos
+        #                                      <tasos.laskos@gmail.com>
+        #                                      <zapotek@segfault.gr>
+        # @version: 0.1
+        #
+        class Profiler < Arachni::Plugin::Formatter
+
+            def initialize( plugin_data )
+                @results     = plugin_data[:results]
+                @description = plugin_data[:description]
+            end
+
+            def run
+
+                times      = @results['times'].map{ |item| item['time'] }
+                total_time = 0
+                times.each {
+                    |time|
+                    total_time += time
+                }
+
+                avg_time = total_time / times.size
+                times.reject!{ |time| time < avg_time }
+
+                return ERB.new( IO.read( File.dirname( __FILE__ ) + '/profiler/template.erb' ) ).result( binding )
+            end
+
+        end
+
+    end
+end
+
+end
+end
