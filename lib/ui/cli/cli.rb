70fe3983346bdbdf8673ece95361f1a22eac689d
Tasos Laskos <tasos.laskos@gmail.com>
2011-03-01 13:39:21 +0000

    updated homepage

--- lib/ui/cli/cli.rb
+++ lib/ui/cli/cli.rb
@@ -554,7 +554,7 @@ class CLI
                                       <zapotek@segfault.gr>
                (With the support of the community and the Arachni Team.)
 
-       Website:       http://github.com/Zapotek/arachni
+       Website:       http://arachni.segfault.gr - http://github.com/Zapotek/arachni
        Documentation: http://github.com/Zapotek/arachni/wiki'
         print_line
         print_line
