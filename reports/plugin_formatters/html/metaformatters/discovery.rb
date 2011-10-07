86682c7ee00c71bbc2638baf4e69db5f3023fdb9
Tasos Laskos <tasos.laskos@gmail.com>
2011-10-07 06:35:36 +0300

    added anomaly detection metamodule (and report formatters) for discovery modules, it analyses the issues logged by them and checks for suspiciously high factors of similarity between them [Closes #81]

---
+++ reports/plugin_formatters/html/metaformatters/discovery.rb
@@ -0,0 +1,65 @@
+=begin
+                  Arachni
+  Copyright (c) 2010-2011 Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
+
+  This is free software; you can copy and distribute and modify
+  this program under the term of the GPL v2.0 License
+  (See LICENSE file for details)
+
+=end
+
+module Arachni
+
+module Reports
+
+class HTML
+module PluginFormatters
+
+class MetaModules
+
+module MetaFormatters
+
+    #
+    # HTML formatter for the results of the Discovery metamodule
+    #
+    # @author: Tasos "Zapotek" Laskos
+    #                                      <tasos.laskos@gmail.com>
+    #                                      <zapotek@segfault.gr>
+    # @version: 0.1
+    #
+    class Discovery < Arachni::Plugin::Formatter
+
+        def initialize( metadata )
+            @results     = metadata[:results]
+            @description = metadata[:description]
+        end
+
+        def run
+            return ERB.new( tpl ).result( binding )
+        end
+
+        def tpl
+            %q{
+                    <h4>Discovery</h4>
+                    <blockquote><pre><%=::Arachni::Reports::HTML.prep_description(@description)%></pre></blockquote>
+
+                    <ul>
+                    <%@results.each do |issue| %>
+                        <li>
+                            <a href="#issue_<%=issue['index']%>">
+                                <%=issue['name']%> at <%=issue['url']%>
+                            </a>
+                        </li>
+                    <%end%>
+                    </ul>
+            }
+        end
+    end
+
+end
+
+end
+end
+end
+end
+end
