3951b9b16e3fc633f7a9c2cf0c138c7d0dab892c
Tasos Laskos <tasos.laskos@gmail.com>
2011-11-02 07:17:17 +0200

    removed xmlrpc server monkey patching since it isn't used anymore

--- lib/ruby.rb
+++ lib/ruby.rb
@@ -1,6 +1,4 @@
-
 opts = Arachni::Options.instance
 require opts.dir['lib'] +  'ruby/object'
 require opts.dir['lib'] +  'ruby/array'
 require opts.dir['lib'] +  'ruby/string'
-require opts.dir['lib'] +  'ruby/xmlrpc/server'
