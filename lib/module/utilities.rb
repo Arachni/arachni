8062feba7a43f6c21b36d1e0c65bcf3194be6700
Tasos Laskos <tasos.laskos@gmail.com>
2011-11-04 08:06:15 +0200

    exception_jail(): print_debug_backtrace =>

--- lib/module/utilities.rb
+++ lib/module/utilities.rb
@@ -155,7 +155,7 @@ module Utilities
         rescue Exception => e
             err_name = !e.to_s.empty? ? e.to_s : e.class.name
             print_error( err_name )
-            print_debug_backtrace( e )
+            print_error_backtrace( e )
             raise e
         end
     end
