5fd203b7551ecfe66c8918d03f07418b463f185c
Tasos Laskos <tasos.laskos@gmail.com>
2011-11-28 23:13:08 +0200

    added the manual verification plugin: identifies issues that require manual verification as anomalous, a classification that will place them in the \"untrusted\" section

---
+++ plugins/defaults/metamodules/remedies/manual_verification.rb
@@ -0,0 +1,62 @@
+=begin
+                  Arachni
+  Copyright (c) 2010-2011 Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
+
+  This is free software; you can copy and distribute and modify
+  this program under the term of the GPL v2.0 License
+  (See LICENSE file for details)
+
+=end
+
+module Arachni
+module Plugins
+
+#
+# Gathers all issues that require manual verification into an array.
+#
+# @author: Tasos "Zapotek" Laskos
+#                                      <tasos.laskos@gmail.com>
+#                                      <zapotek@segfault.gr>
+# @version: 0.1
+#
+class ManualVerification < Arachni::Plugin::Base
+
+    def prepare
+        ::IO.select( nil, nil, nil, 1 ) while( @framework.running? )
+    end
+
+    def run
+        # will hold the hash IDs of inconclusive issues
+        inconclusive = []
+        @framework.audit_store.issues.each.with_index {
+            |issue, idx|
+            next if !issue.verification
+
+            inconclusive << {
+                'hash'   => issue._hash,
+                'index'  => idx + 1,
+                'url'    => issue.url,
+                'name'   => issue.name,
+                'var'    => issue.var,
+                'elem'   => issue.elem,
+                'method' => issue.method
+            }
+        }
+
+        register_results( inconclusive ) if !inconclusive.empty?
+    end
+
+    def self.info
+        {
+            :name           => 'Issues requiring manual verification',
+            :description    => %q{Goes through the list of logged issues and cherry picks the ones that require manual verification.},
+            :author         => 'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>',
+            :version        => '0.1',
+            :tags           => [ 'anomaly' , 'verification', 'meta' ]
+        }
+    end
+
+end
+
+end
+end
