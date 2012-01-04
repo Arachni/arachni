c3bc79fb32b4f0b435c685c36c6256fc657c0dc2
Tasos Laskos <tasos.laskos@gmail.com>
2012-01-04 04:24:18 +0200

    the seed paths now contain the seed url

--- lib/arachni/spider.rb
+++ lib/arachni/spider.rb
@@ -111,7 +111,7 @@ class Spider
 
         parser = Parser.new( @opts, seed_page )
         parser.url = @seed_url
-        @paths = parser.paths
+        @paths = parser.paths | [@seed_url]
 
         while( !@paths.empty? )
             while( !@paths.empty? && url = parser.to_absolute( @paths.pop ) )
