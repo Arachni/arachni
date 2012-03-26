08ae2cf57ffeb00d0e600327cb20e420a921eaeb
Tasos Laskos <tasos.laskos@gmail.com>
2012-03-26 23:21:43 +0300

    added specs for

---
+++ spec/arachni/rpc/server/modules/manager_spec.rb
@@ -0,0 +1,70 @@
+require_relative '../../../../spec_helper'
+
+require Arachni::Options.instance.dir['lib'] + 'rpc/client/instance'
+require Arachni::Options.instance.dir['lib'] + 'rpc/server/instance'
+
+describe Arachni::RPC::Server::Module::Manager do
+    before( :all ) do
+        @opts = Arachni::Options.instance
+        @opts.dir['modules'] = spec_path + 'fixtures/modules/'
+        @opts.rpc_address = 'localhost'
+
+        port1 = random_port
+        port2 = random_port
+
+        @token = 'secret!'
+
+        @pids = []
+        @pids << ::EM::fork_reactor {
+            @opts.rpc_port = port1
+            Arachni::RPC::Server::Instance.new( @opts, @token )
+        }
+        @pids << ::EM::fork_reactor {
+            @opts.rpc_port = port2
+            Arachni::RPC::Server::Instance.new( @opts, @token )
+        }
+        sleep 1
+
+        @modules = Arachni::RPC::Client::Instance.new( @opts,
+            "#{@opts.rpc_address}:#{port1}", @token
+        ).modules
+
+        @modules_clean = Arachni::RPC::Client::Instance.new( @opts,
+            "#{@opts.rpc_address}:#{port2}", @token
+        ).modules
+    end
+
+    after( :all ){
+        @pids.each { |p| Process.kill( 'KILL', p ) }
+        @opts.reset!
+        kill_em!
+    }
+
+    describe :available do
+        it 'should return an array of available modules' do
+            @modules.available.should be_any
+        end
+    end
+
+    describe :loaded do
+        context 'when there are loaded modules' do
+            it 'should return an empty array' do
+                @modules.loaded.should be_empty
+            end
+        end
+        context 'when there are loaded modules' do
+            it 'should return an array of loaded modules' do
+                @modules.load( '*' )
+                @modules.loaded.should be_any
+            end
+        end
+    end
+
+    describe :load do
+        it 'should load modules by name' do
+            @modules_clean.load( 'test' )
+            @modules_clean.loaded.should == ['test']
+        end
+    end
+
+end
