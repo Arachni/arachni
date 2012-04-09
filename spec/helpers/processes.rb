337ae0e9e987e6f570f31f59c5605ca9f6a672a7
zapotek <tasos.laskos@gmail.com>
2012-04-10 01:47:53 +0300

    added process helpers

---
+++ spec/helpers/processes.rb
@@ -0,0 +1,22 @@
+
+def pids
+    @@proc_pids ||= []
+end
+
+def blocks
+    @@blocks ||= {}
+end
+
+def kill_processes!
+    pids.each { |p| blocks.delete( p ); Process.kill( 'KILL', p ) rescue nil }
+end
+
+def fork_proc( *args, &b )
+    pids << fork( *args, &b )
+    blocks[pids.last] = b
+end
+
+def fork_em( *args, &b )
+    pids << ::EM.fork_reactor( *args, &b )
+    blocks[pids.last] = b
+end
