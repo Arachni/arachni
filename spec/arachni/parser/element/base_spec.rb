5ca24aeb944820aeb94e17525fad35c170bbfa55
zapotek <tasos.laskos@gmail.com>
2012-04-23 09:58:30 +0300

    added specs for dup() and ==

--- spec/arachni/parser/element/base_spec.rb
+++ spec/arachni/parser/element/base_spec.rb
@@ -15,4 +15,60 @@ describe Arachni::Parser::Element::Base do
         @e.raw.should == @raw
     end
 
+    describe :== do
+        it 'should assert equality based on method, action and inputs' do
+            e = Arachni::Parser::Element::Link.new( @url, inputs: { 'name' => 'val' } )
+            c = Arachni::Parser::Element::Link.new( @url, inputs: { 'name' => 'val' } )
+            c.should == e
+
+            e = Arachni::Parser::Element::Form.new( @url, inputs: { 'name' => 'val' } )
+            c = Arachni::Parser::Element::Form.new( @url, inputs: { 'name' => 'val' } )
+            c.should == e
+
+            e = Arachni::Parser::Element::Form.new( @url, method: 'post', inputs: { 'name' => 'val' } )
+            c = Arachni::Parser::Element::Form.new( @url, method: 'post', inputs: { 'name' => 'val' } )
+            c.should == e
+
+            e = Arachni::Parser::Element::Form.new( @url, inputs: { 'name' => 'val' } )
+            c = Arachni::Parser::Element::Form.new( @url, method: 'get', inputs: { 'name' => 'val' } )
+            c.should_not == e
+
+            e = Arachni::Parser::Element::Form.new( @url, inputs: { 'name' => 'val' } )
+            c = Arachni::Parser::Element::Form.new( @url + 's', inputs: { 'name' => 'val' } )
+            c.should_not == e
+
+            e = Arachni::Parser::Element::Form.new( @url, inputs: { 'name' => 'val' } )
+            c = Arachni::Parser::Element::Form.new( @url, inputs: { 'name2' => 'val' } )
+            c.should_not == e
+        end
+    end
+
+    describe :dup do
+        before do
+            @elem = Arachni::Parser::Element::Link.new( @url, inputs: { 'name' => 'val' } )
+        end
+        it 'should duplicate the element' do
+            e = @elem.dup
+            e.should == @elem
+            e.url = 'blah'
+            e.auditable['crap'] = 'stuff'
+            @elem.auditable['crap'].should be_nil
+            @elem.url.should == @url
+        end
+        it 'should maintain the scope override' do
+            e = @elem.dup
+            e.override_instance_scope?.should be_false
+            e.override_instance_scope?.should == @elem.override_instance_scope?
+
+            e.override_instance_scope!
+            c = e.dup
+            e.override_instance_scope?.should be_true
+            c.override_instance_scope?.should == e.override_instance_scope?
+
+            a = @elem.dup
+            a.override_instance_scope!
+            a.override_instance_scope?.should_not == @elem.override_instance_scope?
+        end
+    end
+
 end
