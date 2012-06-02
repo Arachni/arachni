ce140fcba0759ac010c904c7b35416a680a499b8
zapotek <tasos.laskos@gmail.com>
2012-06-03 01:52:54 +0300

    added a require for enumerable.rb

--- lib/arachni/ruby.rb
+++ lib/arachni/ruby.rb
@@ -1,4 +1,5 @@
 opts = Arachni::Options.instance
 require opts.dir['lib'] +  'ruby/object'
+require opts.dir['lib'] +  'ruby/enumerable'
 require opts.dir['lib'] +  'ruby/array'
 require opts.dir['lib'] +  'ruby/string'
