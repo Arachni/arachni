66deb0e0c93bfbc8a7eb8f590c4ab16a9fac1ed4
zapotek <tasos.laskos@gmail.com>
2012-06-06 09:33:30 +0300

    added cookiejar specs and docs

--- lib/arachni/http/cookie_jar.rb
+++ lib/arachni/http/cookie_jar.rb
@@ -18,20 +18,32 @@ module Arachni
 class HTTP
 
 #
-# TODO: Implement proper tailmatching
+# Basic CookieJar implementation
+#
+# @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
 #
 class CookieJar
     include Arachni::Utilities
 
+    # Same as {#initialize}.
     def self.from_file( *args )
         new.load( *args )
     end
 
+    # @param    [String]    cookie_jar_file path to a Netscape cookie-jar
     def initialize( cookie_jar_file = nil )
         @domains = {}
         load( cookie_jar_file ) if cookie_jar_file
     end
 
+    #
+    # Loads cookies from a Netscape cookiejar file
+    #
+    # @param    [String]    cookie_jar_file path to a Netscape cookie-jar
+    # @param    [String]    url     cookie owner
+    #
+    # @return   [Arachni::HTTP::CookieJar]  self
+    #
     def load( cookie_jar_file, url = '' )
         # make sure that the provided cookie-jar file exists
         if !File.exist?( cookie_jar_file )
@@ -39,27 +51,53 @@ class CookieJar
                    'Cookie-jar \'' + cookie_jar_file + '\' doesn\'t exist.' )
         end
         update( cookies_from_file( url, cookie_jar_file ) )
+        self
     end
 
+    #
+    # Updates the jar with +cookie+.
+    #
+    # @param    [Arachni::Parser::Element::Cookie]  cookie
+    #
+    # @return   [Arachni::HTTP::CookieJar]  self
+    #
     def <<( cookie )
         ((@domains[cookie.domain] ||= {})[cookie.path] ||= {})[cookie.name] = cookie.dup
+        self
     end
 
+    #
+    # Updates the jar with +cookies+.
+    #
+    # @param    [Array<Arachni::Parser::Element::Cookie>]  cookies
+    #
+    # @return   [Arachni::HTTP::CookieJar]  self
+    #
     def update( cookies )
         [cookies].flatten.compact.each { |c| self << c }
+        self
     end
 
-    def get_cookies( url )
+    #
+    # Gets cookies for a specific +url+.
+    #
+    # @param    [String]    url
+    #
+    # @return   [Array<Arachni::Parser::Element::Cookie>]
+    #
+    def for_url( url )
         uri = to_uri( url )
+        request_path   = uri.path
         request_domain = uri.host
-        request_path = uri.path
 
         return [] if !request_domain || !request_path
 
         @domains.map do |domain, paths|
-            next if (request_domain != domain) && ( '.' + request_domain != domain)
+            next if !in_domain?( domain, request_domain )
+
             paths.map do |path, cookies|
                 next if !request_path.start_with?( path )
+
                 cookies.values.reject{ |c| c.expired? }
             end
         end.flatten.compact.sort do |lhs, rhs|
@@ -67,10 +105,17 @@ class CookieJar
         end
     end
 
-    def cookies( with_expired = false )
+    #
+    # Returns all cookies
+    #
+    # @param    [Bool]  include_expired    include expired cookies
+    #
+    # @return   [Array<Arachni::Parser::Element::Cookie>]
+    #
+    def cookies( include_expired = false )
         @domains.values.map do |paths|
             paths.values.map do |cookies|
-                if !with_expired
+                if !include_expired
                     cookies.values.reject{ |c| c.expired? }
                 else
                     cookies.values
@@ -79,14 +124,43 @@ class CookieJar
         end.flatten.compact
     end
 
+    # Empties the cookiejar
+    def clear
+        @domains.clear
+    end
+
+    # @return   [Bool]  +true+ if cookiejar is empty, +false+ otherwise
+    def empty?
+        @domains.empty?
+    end
+
+    # @return   [Bool]  +true+ if cookiejar is not empty, +false+ otherwise
+    def any?
+        !empty?
+    end
+
+    #
+    # @param    [String]    url     get cookies for +url+
+    #
+    # @return   [String]    cookies as a string formatted for HTTP request headers
+    #
     def to_s( url )
-        get_cookies( url ).map{ |c| c.to_s }.join( ';' )
+        for_url( url ).map{ |c| c.to_s }.join( ';' )
     end
 
     private
 
+    def in_domain?( cookie_domain, request_domain )
+        request_domain == cookie_domain ||
+            ( cookie_domain.start_with?( '.' ) &&
+              request_domain.end_with?( cookie_domain[1...cookie_domain.size] )
+            )
+    end
+
     def to_uri( url )
-        url.is_a?( ::URI ) || url.is_a?( Arachni::URI ) ? url : uri_parse( url.to_s )
+        u = url.is_a?( ::URI ) || url.is_a?( Arachni::URI ) ? url : uri_parse( url.to_s )
+        fail 'Complete absolute URL required.' if u.relative?
+        u
     end
 
 end
