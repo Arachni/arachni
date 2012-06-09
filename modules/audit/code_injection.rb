211b983ceb012bf6d73ddd850982e83ce211defb
zapotek <tasos.laskos@gmail.com>
2012-06-09 15:56:48 +0300

    * code clean-up * now uses lazy loaded class variables instead of initializing instance variables for static data over and over

--- modules/audit/code_injection.rb
+++ modules/audit/code_injection.rb
@@ -15,7 +15,6 @@
 =end
 
 module Arachni
-
 module Modules
 
 #
@@ -23,10 +22,9 @@ module Modules
 # but still needs some more testing.
 #
 #
-# @author Tasos "Zapotek" Laskos
-#                                      <tasos.laskos@gmail.com>
-#                                      
-# @version 0.1.5
+# @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
+#
+# @version 0.1.6
 #
 # @see http://cwe.mitre.org/data/definitions/94.html
 # @see http://php.net/manual/en/function.eval.php
@@ -37,92 +35,80 @@ module Modules
 #
 class CodeInjection < Arachni::Module::Base
 
-    include Arachni::Module::Utilities
-
     def prepare
+        #
+        # Digits from a sha1 hash.
+        #
+        # The codes in @injection_strs will tell the web app to sum them up
+        # and echo the result.
+        #
+        @@rand1 ||= '287630581954'
+        @@rand2 ||= '4196403186331128'
 
-        # digits from a sha1 hash
-        # the codes in @__injection_strs will tell the web app
-        # to sum them and echo the result
-        @__rand1 = '287630581954'
-        @__rand2 = '4196403186331128'
-
-        @__opts = {
-            :substring => ( @__rand1.to_i + @__rand2.to_i ).to_s,
-            :format    => [ Format::APPEND, Format::STRAIGHT ]
+        @@opts ||= {
+            substring: (@@rand1.to_i + @@rand2.to_i).to_s,
+            format:    [Format::APPEND, Format::STRAIGHT],
+            param_flip: false
         }
 
-        # code to be injected to the webapp
-        @__injection_strs = [
-            "echo " + @__rand1 + "+" + @__rand2 + ";", # PHP
-            "print " + @__rand1 + "+" + @__rand2 + ";", # Perl
-            "print " + @__rand1 + " + " + @__rand2, # Python
-            "Response.Write\x28" +  @__rand1  + '+' + @__rand2 + "\x29", # ASP
-            "puts " + @__rand1 + " + " + @__rand2 # Ruby
-        ]
+        # code strings to be injected to the webapp
+        @@code_strings ||= [
+            "echo " + @@rand1 + "+" + @@rand2 + ";", # PHP
+            "print " + @@rand1 + "+" + @@rand2 + ";", # Perl
+            "print " + @@rand1 + "+" + @@rand2, # Python
 
-        @__variations = [
-            ';%s',
-            "\";%s#",
-            "';%s#"
+            # the 2 following will most likely print to the console but give them a shot
+            "Response.Write\x28" +  @@rand1  + '+' + @@rand2 + "\x29", # ASP
+            "puts " + @@rand1 + "+" + @@rand2 # Ruby
         ]
     end
 
     def run
-
-        # iterate through the injection codes
-        @__injection_strs.each {
-            |str|
-            __variations( str ).each {
-                |var|
-                audit( var, @__opts )
-            }
-        }
-
+        generate_variations.each { |var| audit( var, @@opts ) }
     end
 
-    def __variations( str )
-        @__variations.map { |var| uri_encode( var % str, '+' ) } | [uri_encode( str, '+' )]
+    def generate_variations
+        @@variations ||= @@code_strings.map do |str|
+            [ ';%s', "\";%s#", "';%s#" ].map { |var| var % str } | [str]
+        end.flatten.compact
     end
-
 
     def self.info
         {
-            :name           => 'Code injection',
-            :description    => %q{It tries to inject code snippets into the
+            name:        'Code injection',
+            description: %q{It tries to inject code snippets into the
                 web application and assess whether or not the injection
                 was successful.},
-            :elements       => [
+            elements:    [
                 Issue::Element::FORM,
                 Issue::Element::LINK,
                 Issue::Element::COOKIE,
                 Issue::Element::HEADER
             ],
-            :author         => 'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com> ',
-            :version        => '0.1.5',
-            :references     => {
+            author:      'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com> ',
+            version:     '0.1.6',
+            references:  {
                 'PHP'    => 'http://php.net/manual/en/function.eval.php',
                 'Perl'   => 'http://perldoc.perl.org/functions/eval.html',
                 'Python' => 'http://docs.python.org/py3k/library/functions.html#eval',
                 'ASP'    => 'http://www.aspdev.org/asp/asp-eval-execute/',
                 'Ruby'   => 'http://en.wikipedia.org/wiki/Eval#Ruby'
-             },
-            :targets        => { 'Generic' => 'all' },
-
-            :issue   => {
-                :name        => %q{Code injection},
-                :description => %q{Arbitrary code can be injected into the web application
-                    which is then executed as part of the system.},
-                :tags        => [ 'code', 'injection', 'regexp' ],
-                :cwe         => '94',
-                :severity    => Issue::Severity::HIGH,
-                :cvssv2       => '7.5',
-                :remedy_guidance    => %q{User inputs must be validated and filtered
-                    before being evaluated as executable code.
-                    Better yet, the web application should stop evaluating user
-                    inputs as any part of dynamic code altogether.},
-                :remedy_code => '',
-                :metasploitable => 'unix/webapp/arachni_php_eval'
+            },
+            targets:     { 'Generic' => 'all' },
+            issue:       {
+                name:            %q{Code injection},
+                description:     %q{Arbitrary code can be injected into the web application
+    which is then executed as part of the system.},
+                tags:            %w(code injection regexp),
+                cwe:             '94',
+                severity:        Issue::Severity::HIGH,
+                cvssv2:          '7.5',
+                remedy_guidance: %q{User inputs must be validated and filtered
+    before being evaluated as executable code.
+    Better yet, the web application should stop evaluating user
+    inputs as any part of dynamic code altogether.},
+                remedy_code:     '',
+                metasploitable:  'unix/webapp/arachni_php_eval'
             }
 
         }
