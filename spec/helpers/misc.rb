7e10ab44ce53326a4c818a2ec60c8c9637cf8ff0
zapotek <tasos.laskos@gmail.com>
2012-06-20 18:43:11 +0300

    removed #remove_constants

--- spec/helpers/misc.rb
+++ spec/helpers/misc.rb
@@ -14,24 +14,6 @@ def run_http!
     Arachni::HTTP.instance.run
 end
 
-def remove_constants( mod, children_only = false, skip = [] )
-    return if skip.include?( mod )
-    return if !(mod.is_a?( Class ) || mod.is_a?( Module )) ||
-        !mod.to_s.start_with?( 'Arachni' )
-
-    parent = Object
-    mod.to_s.split( '::' )[0..-2].each do |ancestor|
-        parent = parent.const_get( ancestor.to_sym )
-    end
-
-    mod.constants.each do |m|
-        remove_constants( mod.const_get( m ), false, skip )
-    end
-
-    return if children_only
-    parent.send( :remove_const, mod.to_s.split( ':' ).last.to_sym )
-end
-
 def random_port
     loop do
         port = 5555 + rand( 9999 )
