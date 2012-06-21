4e43688fcab5d8fc5f5139e5bb702080c289b215
zapotek <tasos.laskos@gmail.com>
2012-06-21 14:22:16 +0300

    xpan injection module: cleaned up and added specs

--- modules/audit/xpath.rb
+++ modules/audit/xpath.rb
@@ -14,90 +14,53 @@
     limitations under the License.
 =end
 
-module Arachni
-module Modules
-
 #
 # XPath Injection audit module.
 #
-# @author Tasos "Zapotek" Laskos
-#                                      <tasos.laskos@gmail.com>
-#                                      
-# @version 0.1.1
+# @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
+#
+# @version 0.1.2
 #
 # @see http://cwe.mitre.org/data/definitions/91.html
 # @see http://www.owasp.org/index.php/XPATH_Injection
 # @see http://www.owasp.org/index.php/Testing_for_XPath_Injection_%28OWASP-DV-010%29
 #
-class XPathInjection < Arachni::Module::Base
+class Arachni::Modules::XPathInjection < Arachni::Module::Base
 
-    include Arachni::Module::Utilities
+    def self.error_strings
+        @error_strings ||= read_file( 'errors.txt' )
+    end
 
-    def prepare
-
-        #
-        # we make this a class variable and populate it only once
-        # to reduce file IO
-        #
-        @@__errors ||= []
-
-        if @@__errors.empty?
-            read_file( 'errors.txt' ) { |error| @@__errors << error }
-        end
-
-        # prepare the strings that will hopefully cause the webapp
-        # to output XPath error messages
-        @__injection_strs = [
-            "'\"",
-            "<!--"
-        ]
-
-        @__opts = {
-            :format => [ Format::APPEND ],
-            :substring => @@__errors
-        }
-
+    def self.opts
+        @opts ||= { format: [Format::APPEND], substring: error_strings }
     end
 
     def run
-        @__injection_strs.each {
-            |str|
-            audit( str, @__opts )
-        }
+        # these will hopefully cause the webapp to output XPath error messages
+        %w('" ]]]]]]]]] <!--).each { |str| audit( str, self.class.opts ) }
     end
-
 
     def self.info
         {
-            :name           => 'XPathInjection',
-            :description    => %q{XPath injection module},
-            :elements       => [
-                Issue::Element::FORM,
-                Issue::Element::LINK,
-                Issue::Element::COOKIE,
-                Issue::Element::HEADER
-            ],
-            :author         => 'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>',
-            :version        => '0.1.1',
-            :references     => {
-                'OWASP'      => 'http://www.owasp.org/index.php/XPATH_Injection'
+            name:        'XPathInjection',
+            description: %q{XPath injection module},
+            elements:    [ Element::FORM, Element::LINK, Element::COOKIE, Element::HEADER ],
+            author:      'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>',
+            version:     '0.1.2',
+            references:  {
+                'OWASP' => 'http://www.owasp.org/index.php/XPATH_Injection'
             },
-            :targets        => { 'Generic' => 'all' },
-            :issue   => {
-                :name        => %q{XPath Injection},
-                :description => %q{XPath queries can be injected into the web application.},
-                :tags        => [ 'xpath', 'database', 'error', 'injection', 'regexp' ],
-                :cwe         => '91',
-                :severity    => Issue::Severity::HIGH,
-                :cvssv2       => '',
-                :remedy_guidance    => 'User inputs must be validated and filtered
-                    before being included in database queries.',
-                :remedy_code => ''
+            targets:     %w(General PHP Java dotNET Java libXML2),
+            issue:       {
+                name:            %q{XPath Injection},
+                description:     %q{XPath queries can be injected into the web application.},
+                tags:            %w(xpath database error injection regexp),
+                cwe:             '91',
+                severity:        Severity::HIGH,
+                remedy_guidance: 'User inputs must be validated and filtered
+    before being included in database queries.',
             }
-
         }
     end
 
-end
-end
 end
