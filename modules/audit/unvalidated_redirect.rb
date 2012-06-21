442521d86cdc383fed70d224dd67ab92af50ef0b
zapotek <tasos.laskos@gmail.com>
2012-06-21 12:55:06 +0300

    unvalidated_redirect module: cleaned up and added specs

--- modules/audit/unvalidated_redirect.rb
+++ modules/audit/unvalidated_redirect.rb
@@ -14,72 +14,54 @@
     limitations under the License.
 =end
 
-module Arachni
-
-module Modules
-
 #
 # Unvalidated redirect audit module.
 #
 # It audits links, forms and cookies, injects URLs and checks
-# the Location header field to determnine whether the attack was successful.
+# the Location header field to determine whether the attack was successful.
 #
 #
-# @author Tasos "Zapotek" Laskos
-#                                      <tasos.laskos@gmail.com>
-#                                      
-# @version 0.1.3
+# @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
+#
+# @version 0.1.4
 #
 # @see http://www.owasp.org/index.php/Top_10_2010-A10-Unvalidated_Redirects_and_Forwards
 #
-class UnvalidatedRedirect < Arachni::Module::Base
+class Arachni::Modules::UnvalidatedRedirect < Arachni::Module::Base
 
-    def run
-        [
-          'www.arachni-boogie-woogie.com',
-          'http://www.arachni-boogie-woogie.com',
-        ].each {
-            |url|
-            audit( url ) {
-                |res, opts|
-                log( opts, res ) if( res.headers_hash['Location'] == url )
-            }
-        }
+    def self.urls
+        @urls ||= ['www.arachni-boogie-woogie.com',
+                   'https://www.arachni-boogie-woogie.com',
+                   'http://www.arachni-boogie-woogie.com']
     end
 
+    def run
+        self.class.urls.each do |url|
+            audit( url ) { |res, opts| log( opts, res ) if self.class.urls.include?( res.location ) }
+        end
+    end
 
     def self.info
         {
-            :name           => 'UnvalidatedRedirect',
-            :description    => %q{Injects URLs and checks the Location header field
+            name:        'UnvalidatedRedirect',
+            description: %q{Injects URLs and checks the Location header field
                 to determnine whether the attack was successful.},
-            :elements       => [
-                Issue::Element::FORM,
-                Issue::Element::LINK,
-                Issue::Element::COOKIE,
-                Issue::Element::HEADER
-            ],
-            :author         => 'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com> ',
-            :version        => '0.1.3',
-            :references     => {
-                 'OWASP Top 10 2010' => 'http://www.owasp.org/index.php/Top_10_2010-A10-Unvalidated_Redirects_and_Forwards'
+            elements:    [Element::FORM, Element::LINK, Element::COOKIE, Element::HEADER],
+            author:      'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>',
+            version:     '0.1.4',
+            references:  {
+                'OWASP Top 10 2010' => 'http://www.owasp.org/index.php/Top_10_2010-A10-Unvalidated_Redirects_and_Forwards'
             },
-            :targets        => { 'Generic' => 'all' },
+            targets:     %w(Generic),
 
-            :issue   => {
-                :name        => %q{Unvalidated redirect},
-                :description => %q{The web application redirects users to unvalidated URLs.},
-                :tags        => [ 'unvalidated', 'redirect', 'injection', 'header', 'location' ],
-                :cwe         => '819',
-                :severity    => Issue::Severity::MEDIUM,
-                :cvssv2       => '',
-                :remedy_guidance    => '',
-                :remedy_code => '',
+            issue:       {
+                name:            %q{Unvalidated redirect},
+                description:     %q{The web application redirects users to unvalidated URLs.},
+                tags: %w(unvalidated redirect injection header location),
+                cwe:             '819',
+                severity:        Severity::MEDIUM,
             }
-
         }
     end
 
-end
-end
 end
