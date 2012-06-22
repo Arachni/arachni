5885e52c7014959830fa1c77ea0c7b6ecebc2f00
zapotek <tasos.laskos@gmail.com>
2012-06-22 13:04:50 +0300

    cleaned up code

--- plugins/cookie_collector.rb
+++ plugins/cookie_collector.rb
@@ -20,10 +20,9 @@ module Plugins
 #
 # Simple cookie collector
 #
-# @author Tasos "Zapotek" Laskos
-#                                      <tasos.laskos@gmail.com>
-#                                      
-# @version 0.1.3
+# @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
+#
+# @version 0.1.4
 #
 class CookieCollector < Arachni::Plugin::Base
 
@@ -32,10 +31,7 @@ class CookieCollector < Arachni::Plugin::Base
     end
 
     def run
-        @framework.http.add_on_new_cookies {
-            |cookies, res|
-            update( cookies, res )
-        }
+        framework.http.add_on_new_cookies { |cookies, res| update( cookies, res ) }
     end
 
     def update( cookies, res )
@@ -45,28 +41,20 @@ class CookieCollector < Arachni::Plugin::Base
         res_hash.delete( 'body' )
 
         @cookies << {
-            :time       => Time.now,
-            :res        => res_hash,
-            :cookies    => cookies
+            time:    Time.now,
+            res:     res_hash,
+            cookies: cookies
         }
     end
 
     def update?( cookies )
         return true if @cookies.empty?
-
-        cookies.each_pair {
-            |k, v|
-            return true if @cookies.last[:cookies][k] != v
-        }
-
-        return false
+        cookies.each_pair { |k, v| return true if @cookies.last[:cookies][k] != v }
+        false
     end
 
     def clean_up
-        while( @framework.running? )
-            ::IO.select( nil, nil, nil, 1 )
-        end
-
+        wait_while_framework_running
         register_results( @cookies )
     end
 
@@ -78,17 +66,16 @@ class CookieCollector < Arachni::Plugin::Base
         results.flatten
     end
 
-
     def self.info
         {
-            :name           => 'Cookie collector',
-            :description    => %q{Monitors and collects cookies while establishing a timeline of changes.
+            name:        'Cookie collector',
+            description: %q{Monitors and collects cookies while establishing a timeline of changes.
 
                 WARNING: Highly discouraged when the audit includes cookies.
                     It will log thousands of results leading to a huge report,
                     highly increased memory and CPU usage.},
-            :author         => 'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>',
-            :version        => '0.1.3',
+            author:      'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>',
+            version:     '0.1.4',
         }
     end
 
