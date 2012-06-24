79acd6706caf486bf0fae0c1db424af381b8e58c
zapotek <tasos.laskos@gmail.com>
2012-06-24 21:39:03 +0300

    updated filename check

--- modules/recon/backup_files.rb
+++ modules/recon/backup_files.rb
@@ -14,107 +14,65 @@
     limitations under the License.
 =end
 
-module Arachni
-module Modules
-
 #
 # Backup file discovery module.
 #
-# Appends common backup extesions to the filename of the page under audit<br/>
+# Appends common backup extentions to the filename of the page under audit<br/>
 # and checks for its existence.
 #
-# @author Tasos "Zapotek" Laskos
-#                                      <tasos.laskos@gmail.com>
-#                                      
-# @version 0.2.1
+# @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
+#
+# @version 0.2.2
 #
 #
-class BackupFiles < Arachni::Module::Base
+class Arachni::Modules::BackupFiles < Arachni::Module::Base
 
-    include Arachni::Module::Utilities
-
-    def prepare
-        # to keep track of the requests and not repeat them
-        @@__audited ||= Set.new
-
-        @@__extensions ||=[]
-        return if !@@__extensions.empty?
-
-        read_file( 'extensions.txt' ) {
-            |file|
-            @@__extensions << file
-        }
+    def self.extensions
+        @extensions ||= read_file( 'extensions.txt' )
     end
 
     def run
-        path = get_path( @page.url )
-        return if @@__audited.include?( path )
+        path = get_path( page.url )
+        return if audited?( path )
 
-        filename = File.basename( URI( normalize_url( @page.url ) ).path )
+        filename = File.basename( uri_parse( page.url ).path )
 
-        print_status( "Scanning..." )
-
-        if( !filename  )
-            print_info( 'Backing out. ' +
-              'Can\'t extract filename from url: ' + @page.url )
+        if !filename || filename.empty? || filename == '/'
+            print_info "Backing out, couldn't extract filename from: #{page.url}"
             return
         end
 
-        @@__extensions.each {
-            |ext|
-
-            #
-            # Test for the existance of the file + extension.
-            #
-
+        self.class.extensions.each do |ext|
             file = ext % filename # Example: index.php.bak
-            check!( path, file )
+            log_remote_file_if_exists( path + file )
 
             cfile = ext % filename.gsub( /\.(.*)/, '' ) # Example: index.bak
-            check!( path, file ) if file != cfile
-        }
+            log_remote_file_if_exists( path + cfile ) if file != cfile
+        end
 
-        @@__audited << path
-    end
-
-    def check!( path, file )
-
-        url = path + file
-
-        print_status( "Checking for #{url}" )
-
-        log_remote_file_if_exists( url ) {
-            |res|
-            print_ok( "Found #{file} at " + res.effective_url )
-        }
+        audited( path )
     end
 
     def self.info
         {
-            :name           => 'BackupFiles',
-            :description    => %q{Tries to find sensitive backup files.},
-            :elements       => [ ],
-            :author         => 'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com> ',
-            :version        => '0.2.1',
-            :references     => {},
-            :targets        => { 'Generic' => 'all' },
-            :issue   => {
-                :name        => %q{A backup file exists on the server.},
-                :description => %q{ The server response indicates that a file matching
-                    the name of a common naming scheme for file backups can be publicly accessible.
-                    A developer has probably forgotten to remove this file after testing.
-                    This can lead to source code disclosure and privileged information leaks.},
-                :tags        => [ 'path', 'backup', 'file', 'discovery' ],
-                :cew         => '530',
-                :severity    => Issue::Severity::HIGH,
-                :cvssv2       => '',
-                :remedy_guidance    => '',
-                :remedy_code => '',
+            name:        'BackupFiles',
+            description: %q{Tries to find sensitive backup files.},
+            elements:    [ Element::PATH ],
+            author:      'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com> ',
+            version:     '0.2.2',
+            targets:     %w(Generic),
+            issue:       {
+                name:            %q{A backup file exists on the server.},
+                description:     %q{The server response indicates that a file matching
+    the name of a common naming scheme for file backups can be publicly accessible.
+    A developer has probably forgotten to remove this file after testing.
+    This can lead to source code disclosure and privileged information leaks.},
+                tags: %w(path backup file discovery),
+                cew:             '530',
+                severity:        Severity::MEDIUM
             }
 
         }
     end
 
-end
-end
 end
