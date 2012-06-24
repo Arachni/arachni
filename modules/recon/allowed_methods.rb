e1fadbbd2cdeb650c7d25ff28cd13266272c266b
zapotek <tasos.laskos@gmail.com>
2012-06-24 17:38:55 +0300

    allowed_methods module: cleaned up and added specs

--- modules/recon/allowed_methods.rb
+++ modules/recon/allowed_methods.rb
@@ -14,84 +14,62 @@
     limitations under the License.
 =end
 
-module Arachni
-module Modules
-
 #
 # Allowed HTTP methods recon module.
 #
 # @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
 #
-# @version 0.1.3
+# @version 0.1.4
 #
 # @see http://en.wikipedia.org/wiki/WebDAV
 # @see http://www.webdav.org/specs/rfc4918.html
 #
-class AllowedMethods < Arachni::Module::Base
+class Arachni::Modules::AllowedMethods < Arachni::Module::Base
 
-    def prepare
-        @@__ran ||= false
+    def self.ran?
+        !!@ran
+    end
+
+    def self.ran
+        @ran = true
     end
 
     def run
-        return if @@__ran
+        return if self.class.ran?
 
         print_status( "Checking..." )
-
-        http.request( page.url, method: :options ) do |res|
-            __log_results( res )
-        end
+        http.request( page.url, method: :options, remove_id: true ) { |res| check_and_log( res ) }
     end
 
     def clean_up
-        @@__ran = true
+        self.class.ran
     end
 
-    def self.info
-        {
-            :name           => 'AllowedMethods',
-            :description    => %q{Checks for supported HTTP methods.},
-            :elements       => [ ],
-            :author         => 'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>',
-            :version        => '0.1.3',
-            :references     => {
-            },
-            :targets        => { 'Generic' => 'all' },
-            :issue   => {
-                :name        => %q{Allowed HTTP methods},
-                :description => %q{The webserver claims that it supports the logged methods.},
-                :tags        => [ 'http', 'methods', 'options' ],
-                :cwe         => '',
-                :severity    => Issue::Severity::INFORMATIONAL,
-                :cvssv2       => '',
-                :remedy_guidance    => '',
-                :remedy_code => '',
-            }
-        }
-    end
-
-    def __log_results( res )
-
+    def check_and_log( res )
         methods = res.headers_hash['Allow']
-
         return if !methods || methods.empty?
 
-        log_issue(
-            :url          => res.effective_url,
-            :method       => res.request.method.to_s.upcase,
-            :regexp_match => methods,
-            :elem         => Issue::Element::SERVER,
-            :response     => res.body,
-            :headers      => {
-                :request    => res.request.headers,
-                :response   => res.headers,
-            }
-        )
+        log( { element: Element::SERVER, match: methods }, res )
 
         # inform the user that we have a match
         print_ok( methods )
     end
 
-end
-end
+    def self.info
+        {
+            name:        'AllowedMethods',
+            description: %q{Checks for supported HTTP methods.},
+            elements:    [Element::SERVER],
+            author:      'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>',
+            version:     '0.1.4',
+            targets:     %w(Generic),
+            issue:       {
+                name:            %q{Allowed HTTP methods},
+                description:     %q{The webserver claims that it supports the logged methods.},
+                tags:            %w(http methods options),
+                severity:        Severity::INFORMATIONAL
+            }
+        }
+    end
+
 end
