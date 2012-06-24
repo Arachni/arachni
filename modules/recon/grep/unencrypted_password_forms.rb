f8dd3f56e7ed14fc5e8303bcc5a20afac201e31a
zapotek <tasos.laskos@gmail.com>
2012-06-24 17:01:47 +0300

    unencrypted_password_forms module: cleaned up and added specs

--- modules/recon/grep/unencrypted_password_forms.rb
+++ modules/recon/grep/unencrypted_password_forms.rb
@@ -14,101 +14,82 @@
     limitations under the License.
 =end
 
-module Arachni
-module Modules
-
 #
 # Unencrypted password form
 #
 # Looks for password inputs that don't submit data over an encrypted channel (HTTPS).
 #
-# @author Tasos "Zapotek" Laskos
-#                                      <tasos.laskos@gmail.com>
-#                                      
-# @version 0.1.4
+# @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
+#
+# @version 0.1.5
 #
 # @see http://www.owasp.org/index.php/Top_10_2010-A9-Insufficient_Transport_Layer_Protection
 #
-class UnencryptedPasswordForms < Arachni::Module::Base
+class Arachni::Modules::UnencryptedPasswordForms < Arachni::Module::Base
 
-    def prepare
-        @@__audited ||= Set.new
+    def self.audited
+        @audited ||= Set.new
+    end
+
+    def audited?( input )
+        self.class.audited.include?( determine_name( input ) )
+    end
+
+    def audited( input )
+        self.class.audited << determine_name( input )
+    end
+
+    def determine_name( input )
+        input['name'] || input['id']
+    end
+
+    def password?( input )
+        input['type'].to_s.downcase == 'password'
+    end
+
+    def check_form?( form )
+        uri_parse( form.action ).scheme.downcase == 'http' && form.raw['auditable']
     end
 
     def run
-        @page.forms.each {
-            |form|
-            __check( form )
-        }
+        page.forms.each { |form| check_and_log( form ) }
     end
 
-    def __check( form )
+    def check_and_log( form )
+        return if !check_form?( form )
 
-        scheme = URI( form.action ).scheme
-        return if( scheme.downcase == 'https' )
+        form.raw['auditable'].each do |input|
+            name = determine_name( input )
+            next if !password?( input ) || audited?( input ) || !name
 
-        return if !form.raw['auditable']
-        form.raw['auditable'].each {
-            |input|
+            log( var: name, match: name, element: Element::FORM )
 
-            next if !input['type']
-
-            if( input['type'].downcase == 'password' )
-                __log( form.url, input )
-            end
-        }
-    end
-
-    def __log( url, input )
-
-        if @@__audited.include?( input['name'] )
-            print_info( 'Skipping already checked form \'' +
-                input['name'] + '\' of url: ' + url )
-            return
+            print_ok( "Found unprotected password field '#{name}' at #{page.url}" )
+            audited( input )
         end
-
-        name = input['name'] || input['id'] || 'n/a'
-        @@__audited << name
-
-        log_issue(
-            :var          => name,
-            :url          => url,
-            :elem         => Issue::Element::FORM,
-            :response     => @page.html,
-        )
-
-        print_ok( "Found unprotected password field '#{input['name']}' at #{url}" )
-
     end
 
     def self.info
         {
-            :name           => 'UnencryptedPasswordForms',
-            :description    => %q{Looks for password inputs that don't submit data
+            name:        'UnencryptedPasswordForms',
+            description: %q{Looks for password inputs that don't submit data
                 over an encrypted channel (HTTPS).},
-            :elements       => [
-                Issue::Element::FORM
-            ],
-            :author         => 'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com> ',
-            :version        => '0.1.4',
-            :references     => {
+            elements:    [ Element::FORM ],
+            author:      'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com> ',
+            version:     '0.1.5',
+            references:  {
                 'OWASP Top 10 2010' => 'http://www.owasp.org/index.php/Top_10_2010-A9-Insufficient_Transport_Layer_Protection'
             },
-            :targets        => { 'Generic' => 'all' },
-            :issue   => {
-                :name        => %q{Unencrypted password form.},
-                :description => %q{Transmission of password does not use an encrypted channel.},
-                :tags        => [ 'unencrypted', 'password', 'form' ],
-                :cwe         => '319',
-                :severity    => Issue::Severity::MEDIUM,
-                :cvssv2       => '',
-                :remedy_guidance    => '',
-                :remedy_code => '',
+            targets:     %w(Generic),
+            issue:       {
+                name:            %q{Unencrypted password form.},
+                description:     %q{Transmission of password does not use an encrypted channel.},
+                tags:            %w(unencrypted password form),
+                cwe:             '319',
+                severity:        Severity::MEDIUM
             }
 
         }
     end
 
-end
-end
 end
