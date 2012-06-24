3bf2e18ded3da4fa4689e0708674adb83e99567a
zapotek <tasos.laskos@gmail.com>
2012-06-24 18:34:51 +0300

    backdoors module: cleaned up and added specs

--- modules/recon/backdoors.rb
+++ modules/recon/backdoors.rb
@@ -14,84 +14,46 @@
     limitations under the License.
 =end
 
-module Arachni
-module Modules
-
 #
 # Looks for common backdoors on the server.
 #
-# @author Tasos "Zapotek" Laskos
-#                                      <tasos.laskos@gmail.com>
-#                                      
-# @version 0.2.1
+# @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
 #
+# @version 0.2.2
 #
-class Backdoors < Arachni::Module::Base
+class Arachni::Modules::Backdoors < Arachni::Module::Base
 
-    include Arachni::Module::Utilities
-
-    def prepare
-        # to keep track of the requests and not repeat them
-        @@__audited ||= Set.new
-
-        @@__filenames ||=[]
-        return if !@@__filenames.empty?
-
-        read_file( 'filenames.txt' ) {
-            |file|
-            @@__filenames << file
-        }
+    def self.filenames
+        @filenames ||= read_file( 'filenames.txt' )
     end
 
     def run
+        path = get_path( page.url )
+        return if audited?( path )
 
-        path = get_path( @page.url )
-        return if @@__audited.include?( path )
-
-        print_status( "Scanning..." )
-        @@__filenames.each {
-            |file|
-
-            url  = path + file
-
-            print_status( "Checking for #{url}" )
-            log_remote_file_if_exists( url ) {
-                |res|
-                # inform the user
-                print_ok( "Found #{file} at " + res.effective_url )
-            }
-        }
-
-        @@__audited << path
+        self.class.filenames.each { |file| log_remote_file_if_exists( path + file ) }
+        audited( path )
     end
-
 
     def self.info
         {
-            :name           => 'Backdoors',
-            :description    => %q{Tries to find common backdoors on the server.},
-            :elements       => [ ],
-            :author         => 'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com> ',
-            :version        => '0.2.1',
-            :references     => {},
-            :targets        => { 'Generic' => 'all' },
-            :issue   => {
-                :name        => %q{A backdoor file exists on the server.},
-                :description => %q{ The server response indicates that a file matching
-                    the name of a common backdoor is publicly accessible.
-                    This indicates that the server has been compromised and can
-                    (to some extent) be remotely controled by unauthorised users.},
-                :tags        => [ 'path', 'backdoor', 'file', 'discovery' ],
-                :cwe         => '',
-                :severity    => Issue::Severity::HIGH,
-                :cvssv2       => '',
-                :remedy_guidance    => '',
-                :remedy_code => '',
+            name:        'Backdoors',
+            description: %q{Tries to find common backdoors on the server.},
+            elements:    [Element::PATH],
+            author:      'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com> ',
+            version:     '0.2.2',
+            targets:     %w(Generic),
+            issue:       {
+                name:            %q{A backdoor file exists on the server.},
+                description:     %q{ The server response indicates that a file matching
+    the name of a common backdoor is publicly accessible.
+    This indicates that the server has been compromised and can
+    (to some extent) be remotely controled by unauthorised users.},
+                tags:            %w(path backdoor file discovery),
+                severity:        Severity::HIGH
             }
 
         }
     end
 
-end
-end
 end
