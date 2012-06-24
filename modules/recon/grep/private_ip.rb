5ef57ab5b3c9d94b30398e3538badeab32164f21
zapotek <tasos.laskos@gmail.com>
2012-06-24 16:24:51 +0300

    private_ip module: cleaned up and added specs

--- modules/recon/grep/private_ip.rb
+++ modules/recon/grep/private_ip.rb
@@ -14,43 +14,41 @@
     limitations under the License.
 =end
 
-module Arachni
-module Modules
 
 #
 # Private IP address recon module.
 #
-# Scans every page for prvate IP addresses.
+# Scans for private IP addresses.
 #
 # @author   Tasos Laskos <tasos.laskos@gmail.com>
-# @version   0.2
+# @version  0.2.1
 #
-class PrivateIP < Arachni::Module::Base
+class Arachni::Modules::PrivateIP < Arachni::Module::Base
+
+    def self.regexp
+        @regexp ||= /(?<!\.)(?<!\d)(?:(?:10|127)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|192\.168|169\.254|172\.0?(?:1[6-9]|2[0-9]|3[01]))(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){2}(?!\d)(?!\.)/
+    end
 
     def run
-        regexp = /(?<!\.)(?<!\d)(?:(?:10|127)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|192\.168|169\.254|172\.0?(?:1[6-9]|2[0-9]|3[01]))(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){2}(?!\d)(?!\.)/
-        match_and_log( regexp )
+        match_and_log( self.class.regexp )
     end
 
     def self.info
         {
-            :name           => 'Private IP address finder',
-            :description    => %q{Scans pages for private IP addresses.},
-            :author         => 'Tasos Laskos <tasos.laskos@gmail.com>',
-            :version        => '0.2',
-            :targets        => { 'Generic' => 'all' },
-            :issue   => {
-                :name        => %q{Private IP address disclosure.},
-                :description => %q{A private IP address is disclosured in the body of the HTML page},
-                :cwe         => '200',
-                :severity    => Issue::Severity::LOW,
-                :cvssv2      => '0',
-                :remedy_guidance    => %q{Remove private IP addresses from the body of the HTML pages.},
-                :remedy_code => '',
+            name:        'Private IP address finder',
+            description: %q{Scans pages for private IP addresses.},
+            elements:    [],
+            author:      'Tasos Laskos <tasos.laskos@gmail.com>',
+            version:     '0.2.1',
+            targets:     %w(Generic),
+            issue:       {
+                name:            %q{Private IP address disclosure.},
+                description:     %q{A private IP address is disclosed in the body of the HTML page},
+                cwe:             '200',
+                severity:        Severity::LOW,
+                remedy_guidance: %q{Remove private IP addresses from the body of the HTML pages.},
             }
         }
     end
 
-end
-end
 end
