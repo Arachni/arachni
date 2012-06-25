ba275f9a559245ae57fe221579e5c44fc46518d1
zapotek <tasos.laskos@gmail.com>
2012-06-25 17:25:29 +0300

    directory_listing module: cleaned up and added specs

--- modules/recon/directory_listing.rb
+++ modules/recon/directory_listing.rb
@@ -14,9 +14,6 @@
     limitations under the License.
 =end
 
-module Arachni
-module Modules
-
 #
 # Tries to force directory listings.
 #
@@ -24,54 +21,44 @@ module Modules
 #
 # @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
 #
-# @version 0.1.3
+# @version 0.1.4
 #
-class DirectoryListing < Arachni::Module::Base
+class Arachni::Modules::DirectoryListing < Arachni::Module::Base
 
-    DIFF_THRESHOLD = 1000
+    # The compared pages must be at least 75% different
+    DIFF_THRESHOLD = 0.75
 
-    def prepare
-        foo = File.basename( __FILE__, '.rb' )
-        @dirs = [
-            "\\.#{foo}\\",
-            "\\.\\",
-            ".#{foo}/",
-            "./"
-       ]
-
-       @@__checked ||= Set.new
+    def self.dirs
+        @dirs ||= [ "\\.#{seed}\\", "\\.\\", ".#{seed}/", "./" ]
     end
 
     def run
         return if page.code != 200
         path = get_path( page.url )
 
-        parsed = uri_parse( path )
-        return if !parsed.path || parsed.path.gsub( '/', '' ).empty?
-
-        # no redundant checks pl0x! kthxb.
-        return if @@__checked.include?( path )
+        parsed_path = uri_parse( path ).path
+        return if parsed_path == '/' || audited?( parsed_path )
 
         @harvested = []
 
-        @dirs = [ @page.url ] | @dirs.map { |dir| path + dir } | [ path ]
-        @dirs.each_with_index do |url, i|
-            http.get( url ) do |res|
+        dirs = [ page.url ] | self.class.dirs.map { |dir| path + dir } | [ path ]
+        dirs.each_with_index do |url, i|
+            http.get( url, remove_id: true ) do |res|
                 next if !res
                 @harvested[i] = res
-                __check( path ) if __done_harvesting?
+                check_and_log( path ) if done_harvesting?
             end
         end
     end
 
-    def __done_harvesting?
+    def done_harvesting?
         return false if @harvested.size != 6
         @harvested.each { |res| return false if !res }
         true
     end
 
-    def __check( path )
-        @@__checked << path
+    def check_and_log( path )
+        audited( path )
 
         # if we have a 403 Forbidden it means that we successfully
         # built a pah which would force a directory listing *but*
@@ -80,69 +67,42 @@ class DirectoryListing < Arachni::Module::Base
         @harvested.each { |res| return if res.code == 403 }
 
         if !File.basename( @harvested[0].effective_url, '?*' ).empty? &&
-            __same_page?( @harvested[0], @harvested[5] )
+            same_page?( @harvested[0], @harvested[5] )
             return
         end
 
-
-        if !__same_page?( @harvested[1], @harvested[0] ) &&
-           !__same_page?( @harvested[1], @harvested[2] )
-            __log_results( @harvested[5] )
+        if same_page?( @harvested[1], @harvested[0] )  ||
+            same_page?( @harvested[1], @harvested[2] ) ||
+            same_page?( @harvested[3], @harvested[0] ) ||
+            same_page?( @harvested[3], @harvested[4] ) ||
+            @harvested[5].code != 200 || @harvested[5].body.empty?
+            return
         end
 
-        if !__same_page?( @harvested[3], @harvested[0] ) &&
-           !__same_page?( @harvested[3], @harvested[4] )
-            __log_results( @harvested[5] )
-        end
+        log( { element: Element::SERVER }, @harvested[5] )
     end
 
-    def __same_page?( res1, res2 )
-        # back out...
-        return false if res1.code != res2.code
-        return false if (res1.body.size - res2.body.size).abs > DIFF_THRESHOLD
-        true
+    def same_page?( res1, res2 )
+        res1.code == res2.code && res1.body.diff_ratio( res2.body ) <= DIFF_THRESHOLD
     end
 
     def self.info
         {
-            :name           => 'Directory listing',
-            :description    => %q{Tries to force directory listings.},
-            :elements       => [ ],
-            :author         => 'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>',
-            :version        => '0.1.3',
-            :references     => {},
-            :targets        => { 'Generic' => 'all' },
-            :issue   => {
-                :name        => %q{Directory listing is enabled.},
-                :description => %q{In most circumstances enabling directory listings is a bad practise
-                    as it allows an attacker to better grasp the web application's structure.},
-                :tags        => [ 'path', 'directory', 'listing', 'index' ],
-                :cwe         => '548',
-                :severity    => Issue::Severity::LOW,
-                :cvssv2       => '',
-                :remedy_guidance    => '',
-                :remedy_code => '',
+            name:        'Directory listing',
+            description: %q{Tries to force directory listings.},
+            elements:    [ Element::SERVER ],
+            author:      'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>',
+            version:     '0.1.4',
+            targets:     %w(Generic),
+            issue:       {
+                name:        %q{Directory listing is enabled.},
+                description: %q{In most circumstances enabling directory listings is a bad practise
+    as it allows an attacker to better grasp the web application's structure.},
+                tags:        %w(path directory listing index),
+                cwe:         '548',
+                severity:    Severity::LOW
             }
         }
     end
 
-    def __log_results( res )
-        return if res.code != 200 || res.body.empty?
-
-        log_issue(
-            :url          => res.effective_url,
-            :method       => res.request.method.to_s.upcase,
-            :elem         => Issue::Element::SERVER,
-            :response     => res.body,
-            :headers      => {
-                :request    => res.request.headers,
-                :response   => res.headers,
-            }
-        )
-
-        print_ok( 'Found: ' + res.effective_url )
-    end
-
-end
-end
 end
