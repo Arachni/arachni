4b60b9298277387f2b78196a5a0b351b1665e0ee
zapotek <tasos.laskos@gmail.com>
2012-07-02 15:34:04 +0300

    clean_up: lazy chained assignment didn't work properly, broke it up

--- lib/arachni/framework.rb
+++ lib/arachni/framework.rb
@@ -516,7 +516,9 @@ class Framework
     def clean_up( skip_audit_queue = false )
         @status = :cleanup
 
-        @opts.start_datetime ||= @opts.finish_datetime = Time.now
+        @opts.finish_datetime  = Time.now
+        @opts.start_datetime ||= Time.now
+
         @opts.delta_time = @opts.finish_datetime - @opts.start_datetime
 
         # make sure this is disabled or it'll break report output
