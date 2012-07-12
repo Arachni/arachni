ce4a51fdf4e31bee85357c9f7275537cc3afd706
Tasos Laskos <tasos.laskos@gmail.com>
2012-07-13 02:55:59 +0300

    beep_notify  plugin: cleaned up

--- plugins/beep_notify.rb
+++ plugins/beep_notify.rb
@@ -14,44 +14,36 @@
     limitations under the License.
 =end
 
-module Arachni
-module Plugins
-
 #
 # Beeps when the scan finishes.
 #
-# @author Tasos "Zapotek" Laskos
-#                                      <tasos.laskos@gmail.com>
-#                                      
-# @version 0.1
+# @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
 #
-class BeepNotify < Arachni::Plugin::Base
+# @version 0.1.1
+#
+class Arachni::Plugins::BeepNotify < Arachni::Plugin::Base
 
     def run
         wait_while_framework_running
-        @options['repeat'].times {
-            sleep @options['interval']
-            print_info( "Beep!" )
+        options['repeat'].times {
+            sleep options['interval']
+            print_info 'Beep!'
             print 7.chr
         }
-
     end
 
     def self.info
         {
-            :name           => 'Beep notify',
-            :description    => %q{It beeps when the scan finishes.},
-            :author         => 'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>',
-            :version        => '0.1',
-            :options        => [
-                Arachni::OptInt.new( 'repeat', [ false, 'How many times to beep.', 4 ] ),
-                Arachni::OptFloat.new( 'interval', [ false, 'How long to wait between beeps.', 0.4 ] ),
-            ]
+	        name: 'Beep notify',
+	        description: %q{It beeps when the scan finishes.},
+	        author: 'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>',
+	        version: '0.1',
+	        options: [
+		        Arachni::Component::Options::Int.new('repeat', [false, 'How many times to beep.', 4]),
+		        Arachni::Component::Options::Float.new('interval', [false, 'How long to wait between beeps.', 0.4]),
+	        ]
 
         }
     end
 
-end
-
-end
 end
