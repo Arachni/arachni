ed2c83fe0b3d2c50446e1e0b63d1286f57001cc8
Tasos Laskos <tasos.laskos@gmail.com>
2012-07-13 02:41:43 +0300

    added #results_for

--- spec/shared/plugin.rb
+++ spec/shared/plugin.rb
@@ -20,7 +20,11 @@ shared_examples_for "plugin" do
     end
 
     def actual_results
-        (framework.plugins.results[name] || {})[:results]
+	    results_for( name )
+    end
+
+    def results_for( name )
+	    (framework.plugins.results[name] || {})[:results]
     end
 
     def expected_results
