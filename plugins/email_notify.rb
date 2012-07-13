b80e90c0f5568abe5db21e0104c94c609eaa7cf3
Tasos Laskos <tasos.laskos@gmail.com>
2012-07-13 04:12:32 +0300

    added pony gem to the gemspec since the email_notify plugin needs it

--- plugins/email_notify.rb
+++ plugins/email_notify.rb
@@ -14,8 +14,7 @@
     limitations under the License.
 =end
 
-module Arachni
-module Plugins
+require 'pony'
 
 #
 # Uses the Pony gem send a notification (and optionally report) at the end
@@ -23,49 +22,49 @@ module Plugins
 #
 # @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
 #
-# @version 0.1.1
+# @version 0.1.2
 #
-class EmailNotify < Arachni::Plugin::Base
+class Arachni::Plugins::EmailNotify < Arachni::Plugin::Base
 
     def run
         wait_while_framework_running
-        @audit_store = @framework.audit_store
+        @auditstore = framework.auditstore
     end
 
     def clean_up
-        issue_cnt = @audit_store.issues.size
-        time      = @audit_store.delta_time
-        url       = @framework.opts.url
+        issue_cnt = @auditstore.issues.size
+        time      = @auditstore.delta_time
+        url       = framework.opts.url
 
         opts = {
             subject:     "Scan for #{url} finished in #{time}",
             body:        "Found #{issue_cnt} unique issues.",
-            to:          @options['to'],
-            cc:          @options['cc'],
-            bcc:         @options['bcc'],
-            from:        @options['from'],
+            to:          options['to'],
+            cc:          options['cc'],
+            bcc:         options['bcc'],
+            from:        options['from'],
             via:         :smtp,
             via_options: {
-                address:              @options['server_address'],
-                port:                 @options['server_port'],
-                enable_starttls_auto: @options['tls'],
-                user_name:            @options['username'],
-                password:             @options['password'],
-                authentication:       !@options['authentication'].empty? ? @options['authentication'].to_sym : nil,
+                address:              options['server_address'],
+                port:                 options['server_port'],
+                enable_starttls_auto: options['tls'],
+                user_name:            options['username'],
+                password:             options['password'],
+                authentication:       !options['authentication'].empty? ? options['authentication'].to_sym : nil,
                 domain:               "localhost.localdomain"
             }
         }
 
-        if @options['report'] != 'none'
-            report = @framework.reports[ @options['report'] ]
+        if options['report'] != 'none'
+            report = framework.reports[ options['report'] ]
 
             rep_opts = {}
             report.info[:options].each do |opt|
                 rep_opts[opt.name] = opt.default if opt.default
             end
 
-            rep_opts['outfile'] = 'scan_report.' + @options['report']
-            report.new( @audit_store, rep_opts ).run
+            rep_opts['outfile'] = 'scan_report.' + options['report']
+            report.new( @auditstore, rep_opts ).run
 
             opts[:attachments] = {
                 rep_opts['outfile'] => File.read( rep_opts['outfile'] )
@@ -74,40 +73,34 @@ class EmailNotify < Arachni::Plugin::Base
             FileUtils.rm( rep_opts['outfile'] )
         end
 
-        print_status( 'Sending the notification...' )
-        Pony.mail( opts )
-        print_status( 'Done.' )
-    end
+        print_status 'Sending the notification...'
 
-    def self.gems
-        %w(pony)
+        Pony.mail( opts )
+
+        print_status 'Done.'
     end
 
     def self.info
         {
-            :name           => 'E-mail notify',
-            :description    => %q{Sends a notification (and optionally a report) over SMTP
-                at the end of the scan.},
-            :author         => 'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>',
-            :version        => '0.1.1',
-            :options        => [
-                Arachni::OptString.new( 'to', [ true, 'E-mail address of the receiver.' ] ),
-                Arachni::OptString.new( 'cc', [ false, 'E-mail address to which to send a carbon copy of the notification.' ] ),
-                Arachni::OptString.new( 'bcc', [ false, 'E-mail address for a blind carbon copy.' ] ),
-                Arachni::OptString.new( 'from', [ true, 'E-mail address of the sender.' ] ),
-                Arachni::OptAddress.new( 'server_address', [ true, 'Address of the SMTP server to use.' ] ),
-                Arachni::OptPort.new( 'server_port', [ true, 'SMTP port.' ] ),
-                Arachni::OptBool.new( 'tls', [ false, 'Use TLS/SSL?.' ] ),
-                Arachni::OptString.new( 'username', [ true, 'SMTP username.' ] ),
-                Arachni::OptString.new( 'password', [ true, 'SMTP password.' ] ),
-                Arachni::OptString.new( 'authentication', [ false, 'Authentication.', 'plain', [ 'plain', 'login', 'cram_md5', '' ] ] ),
-                Arachni::OptEnum.new( 'report', [ false, 'Report type to send as an attachment.', 'txt', [ 'txt', 'xml', 'html', 'json', 'yaml', 'marshal' 'none' ] ] )
-            ]
+	        name: 'E-mail notify',
+	        description: %q{Sends a notification (and optionally a report) over SMTP at the end of the scan.},
+	        author: 'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>',
+	        version: '0.1.2',
+	        options: [
+		        Arachni::OptString.new('to', [true, 'E-mail address of the receiver.']),
+		        Arachni::OptString.new('cc', [false, 'E-mail address to which to send a carbon copy of the notification.']),
+		        Arachni::OptString.new('bcc', [false, 'E-mail address for a blind carbon copy.']),
+		        Arachni::OptString.new('from', [true, 'E-mail address of the sender.']),
+		        Arachni::OptAddress.new('server_address', [true, 'Address of the SMTP server to use.']),
+		        Arachni::OptPort.new('server_port', [true, 'SMTP port.']),
+		        Arachni::OptBool.new('tls', [false, 'Use TLS/SSL?.']),
+		        Arachni::OptString.new('username', [true, 'SMTP username.']),
+		        Arachni::OptString.new('password', [true, 'SMTP password.']),
+		        Arachni::OptString.new('authentication', [false, 'Authentication.', 'plain', ['plain', 'login', 'cram_md5', '']]),
+		        Arachni::OptEnum.new('report', [false, 'Report type to send as an attachment.', 'txt', ['txt', 'xml', 'html', 'json', 'yaml', 'marshal' 'none']])
+	        ]
 
         }
     end
 
-end
-
-end
 end
