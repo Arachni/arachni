bcb2f971ac54010f0e49903898e9fc2e07bdb3ae
Tasos Laskos <tasos.laskos@gmail.com>
2012-07-15 01:34:10 +0300

    waf_detector plugin: cleaned up and added specs

---
+++ spec/servers/plugins/waf_detector.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+
+def normal_response
+    'Usual response...normal stuff'
+end
+
+def rejected
+    'Piss off!'
+end
+
+def random
+    (0..100).map{ rand( 9999 ).to_s }.join
+end
+
+@@request_cnt ||= 0
+
+get '/positive' do
+    params.to_s.include?( 'script' ) ? rejected : normal_response
+end
+
+get '/negative' do
+    normal_response
+end
+
+get '/inconclusive' do
+    @@request_cnt += 1
+    @@request_cnt % 2 == 0 ? rejected : normal_response
+end
