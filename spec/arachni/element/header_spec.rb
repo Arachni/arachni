4a34e8aac28587fe731b4b147f7bfa88c3bd2e34
Tasos Laskos <tasos.laskos@gmail.com>
2012-08-21 02:27:57 +0300

    tiny formatting update

--- spec/arachni/element/header_spec.rb
+++ spec/arachni/element/header_spec.rb
@@ -1,7 +1,8 @@
 require_relative '../../spec_helper'
 
 describe Arachni::Element::Header do
-    it_should_behave_like 'auditable', url: server_url_for( :header ), single_input: true
+    it_should_behave_like 'auditable', url: server_url_for( :header ),
+                          single_input: true
 
     before( :all ) do
         @url = server_url_for( :header )
