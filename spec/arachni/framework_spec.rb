8055a05eddcdf6008d98dad7ea2d925e18138e10
Tasos Laskos <tasos.laskos@gmail.com>
2012-09-08 07:34:51 +0300

    brought spec example up to date

--- spec/arachni/framework_spec.rb
+++ spec/arachni/framework_spec.rb
@@ -669,7 +669,7 @@ describe Arachni::Framework do
             @f.page_queue_total_size.should == 0
             @f.push_to_page_queue( page )
             @f.run
-            @f.auditstore.issues.size.should == 2
+            @f.auditstore.issues.size.should == 3
             @f.page_queue_total_size.should > 0
             @f.modules.clear
         end
