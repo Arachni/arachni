314ceed8953ec6bd4dd2aa705ee2384a4a1158cc
Tasos Laskos <tasos.laskos@gmail.com>
2012-10-31 14:35:16 +0200

    CSRF module: Updated to mark forms with a detected nonce as safe.

--- spec/servers/modules/audit/csrf.rb
+++ spec/servers/modules/audit/csrf.rb
@@ -78,3 +78,22 @@ get '/token_in_action' do
 
     html
 end
+
+get '/with_nonce' do
+    html = common
+
+    if logged_in?( cookies )
+        html << <<-HTML
+        <form name='insecure_important_form' action='?'>
+            <input name='hooa!' value='important stuff' />
+        </form>
+
+        <form name='secure_important_form' action='?'>
+            <input name='booya!' value='other important stuff' />
+            <input type='hidden' name='my_nonce' value='#{rand(999)}' />
+        </form>
+        HTML
+    end
+
+    html
+end
