3cc2cf5168b7e44e80dc112c0a022d19f96bd7a1
Tasos Laskos <tasos.laskos@gmail.com>
2013-01-18 23:32:58 +0200

    module share spec example: http.headers['User-Agent'] => options.user_agent

--- spec/shared/module.rb
+++ spec/shared/module.rb
@@ -20,7 +20,7 @@ shared_examples_for "module" do
         # do not dedup, the module tests need to see everything
         current_module.instance_eval { define_method( :skip? ) { |elem| false } }
 
-        http.headers['User-Agent'] = 'default'
+        options.user_agent = 'arachni_user'
 
         @issues = []
         Arachni::Module::Manager.do_not_store
