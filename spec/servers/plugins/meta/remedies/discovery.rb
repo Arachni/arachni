4137802ab7c1a5c79e7b27312c143cd4a562ea23
Tasos Laskos <tasos.laskos@gmail.com>
2013-01-23 16:46:36 +0200

    require is now passed an expanded path

--- spec/servers/plugins/meta/remedies/discovery.rb
+++ spec/servers/plugins/meta/remedies/discovery.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-require File.dirname( __FILE__ ) + '/../../../modules/module_server'
+require File.expand_path( File.dirname( __FILE__ ) + '/../../../modules/module_server' )
 
 framework.modules[:common_files].filenames.each do |name|
     get( "/#{name}" ) { 'stuff' }
