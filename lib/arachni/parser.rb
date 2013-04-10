6d8e9b6be7c586c25df52a28c7f84c662b2d687e
Tasos Laskos <tasos.laskos@gmail.com>
2013-04-10 03:16:45 +0300

    Parser: only try to load the cookie_jar file if it exists

--- lib/arachni/parser.rb
+++ lib/arachni/parser.rb
@@ -183,7 +183,7 @@ class Parser
 
         # if there's a Netscape cookiejar file load cookies from it but only new ones,
         # i.e. only if they weren't in the response
-        if @opts.cookie_jar
+        if @opts.cookie_jar.is_a?( String ) && File.exists?( @opts.cookie_jar )
             from_jar |= cookies_from_file( @url, @opts.cookie_jar )
                 .reject { |c| cookie_names.include?( c.name ) }
         end
