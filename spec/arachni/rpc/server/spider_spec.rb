6c3914f01488579f7e73f93fe7643dd597cd4109
Tasos Laskos <tasos.laskos@gmail.com>
2013-05-05 01:12:56 +0300

    RPC::Server::Spider: spec cleanup

--- spec/arachni/rpc/server/spider_spec.rb
+++ spec/arachni/rpc/server/spider_spec.rb
@@ -10,26 +10,26 @@ describe Arachni::RPC::Server::Spider do
                 instance.service.scan(
                     url:            web_server_url_for( :spider ) + '/lots_of_paths',
                     spawns:         4,
-                    http_req_limit: 5,
                     plugins:        %w(spider_hook)
                 )
 
                 sleep 1 while instance.service.busy?
-                instance.framework.clean_up
 
                 instances = instance.service.progress( with: :instances )['instances']
 
                 instances.size.should == 5
-                instances.each { |i| i['sitemap_size'].should > 0 }
+                instances.each { |i| i['sitemap_size'].should > 1 }
 
                 sitemap = instance.spider.sitemap
                 sitemap.size.should == 10051
 
-                plugin_urls = instance.service.
-                    report['plugins']['spider_hook'][:results].values.flatten
-                sitemap.sort.should == plugin_urls.uniq.sort
+                report = instance.service.report
+
+                plugin_urls = report['plugins']['spider_hook'][:results].values.flatten
+                plugin_urls.uniq.sort.should == sitemap.sort
             end
         end
+
         context 'a single node' do
             it 'performs a crawl' do
                 instance = instance_spawn
@@ -38,10 +38,7 @@ describe Arachni::RPC::Server::Spider do
                     url:     web_server_url_for( :spider ) + '/lots_of_paths',
                     plugins: %w(spider_hook)
                 )
-
                 sleep 1 while instance.service.busy?
-
-                instance.framework.clean_up
 
                 progress = instance.service.progress( with: :instances )
 
@@ -53,9 +50,10 @@ describe Arachni::RPC::Server::Spider do
                 sitemap = instance.spider.sitemap
                 sitemap.size.should == progress['stats']['sitemap_size']
 
-                plugin_urls = instance.service.
-                    report['plugins']['spider_hook'][:results].values.flatten
-                sitemap.sort.should == plugin_urls.uniq.sort
+                report = instance.service.report
+
+                plugin_urls = report['plugins']['spider_hook'][:results].values.flatten
+                plugin_urls.uniq.sort.should == sitemap.sort
             end
         end
     end
