a3e1321bcd3121ef7a1e456e4733aa30ee3d7ca5
Tasos Laskos <tasos.laskos@gmail.com>
2013-05-05 05:26:40 +0300

    Processes::Manager: Added the option to not discard output

--- lib/arachni/processes/manager.rb
+++ lib/arachni/processes/manager.rb
@@ -20,59 +20,92 @@ require 'eventmachine'
 module Arachni
 module Processes
 
+#
+# Helper for managing processes.
+#
+# @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
+#
 class Manager
     include Singleton
 
+    # @return   [Array<Integer>] PIDs of all running processes.
     attr_reader :pids
 
     def initialize
-        @pids = []
+        @pids           = []
+        @discard_output = true
     end
 
+    #
+    # @param    [Integer]   pid
+    #   Adds a PID to the {#list} and detaches the process.
+    #
+    # @return   [Integer]   `pid`
+    #
     def <<( pid )
         @pids << pid
         Process.detach pid
         pid
     end
 
+    # @param    [Integer]   pid PID of the process to kill.
     def kill( pid )
         loop do
             begin
                 Process.kill( 'KILL', pid )
             rescue Errno::ESRCH
                 @pids.delete pid
-                return true
             end
         end
     end
 
+    # @param    [Array<Integer>]   pids PIDs of the process to {#kill}.
     def kill_many( pids )
         pids.each { |pid| kill pid }
     end
 
+    # Kills all {#processes}.
     def killall
         kill_many @pids.dup
         @pids.clear
     end
 
-    def quite_fork( &block )
-        self << fork( &discard_output( &block ) )
-    end
-
+    # Stops the EventMachine reactor.
     def kill_em
         ::EM.stop while ::EM.reactor_running? && sleep( 0.1 )
     rescue
         nil
     end
 
+    # @param    [Block] block   Block to fork and discard its output.
+    def quite_fork( &block )
+        self << fork( &discard_output( &block ) )
+    end
+
+    # @param    [Block] block
+    #   Block to fork and run inside EventMachine's reactor thread -- its output
+    #   will be discarded..
     def fork_em( *args, &block )
         self << ::EM.fork_reactor( *args, &discard_output( &block ) )
     end
 
+    # Overrides the default setting of discarding process outputs.
+    def preserve_output
+        @discard_output = false
+    end
+
+    # @param    [Block] block   Block to run silently.
     def discard_output( &block )
+        if !block_given?
+            @discard_output = true
+            return
+        end
+
         proc do
-            $stdout.reopen( '/dev/null', 'w' )
-            $stderr.reopen( '/dev/null', 'w' )
+            if @discard_output
+                $stdout.reopen( '/dev/null', 'w' )
+                $stderr.reopen( '/dev/null', 'w' )
+            end
             block.call
         end
     end
