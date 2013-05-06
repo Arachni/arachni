c5f9e37884c945f871f1aad3cdf82e35075035fd
Tasos Laskos <tasos.laskos@gmail.com>
2013-05-06 15:44:11 +0300

    plugin specs: fixed require path to module_sever

--- spec/support/servers/plugins/meta/remedies/discovery.rb
+++ spec/support/servers/plugins/meta/remedies/discovery.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-require File.expand_path( File.dirname( __FILE__ ) + '/../../../modules/module_server' )
+require_relative '../../../modules/module_server'
 
 framework.modules[:common_files].filenames.each do |name|
     get( "/#{name}" ) { 'stuff' }
