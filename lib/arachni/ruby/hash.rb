0c66517bebb2b65d35fd1e258f62f4c03029425a
Tasos Laskos <tasos.laskos@gmail.com>
2013-05-22 05:13:42 +0300

    Hash#downcase: Don't try to downcase non-Strings

--- lib/arachni/ruby/hash.rb
+++ lib/arachni/ruby/hash.rb
@@ -51,7 +51,12 @@ class Hash
     # @return   [Hash]
     #   Self with the keys and values converted to lower-case strings.
     def downcase
-        stringify_keys.inject({}) { |h, (k, v)| h[k.downcase] = v.downcase; h }
+        stringify_keys.inject({}) do |h, (k, v)|
+            k = k.downcase if k.is_a?( String )
+            v = v.downcase if v.is_a?( String )
+            h[k] = v
+            h
+        end
     end
 
 end
