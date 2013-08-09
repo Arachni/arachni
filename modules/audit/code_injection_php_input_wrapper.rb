164620660b5d78d577a7fbe15c8951e9513e0e32
Tasos Laskos <tasos.laskos@gmail.com>
2013-08-09 23:13:16 +0300

    code_execution_php_input_wrapper audit module: Added payload with nullbyte
    [Issue #373]

--- modules/audit/code_injection_php_input_wrapper.rb
+++ modules/audit/code_injection_php_input_wrapper.rb
@@ -25,7 +25,23 @@ class Arachni::Modules::CodeExecutionPHPInputWrapper < Arachni::Module::Base
         @options ||= {
             format:    [Format::STRAIGHT],
             body:      "<?php echo 'vDBVBsbVdv'; ?> <?php echo chr(80).chr(76).chr(76).chr(33).chr(56).chr(111).chr(55) ?>",
-            substring: 'vDBVBsbVdv PLL!8o7'
+            substring: 'vDBVBsbVdv PLL!8o7',
+
+            # Add one more mutation (on the fly) which will include the extension
+            # of the original value (if that value was a filename) after a null byte.
+            each_mutation: proc do |mutation|
+                m = mutation.dup
+
+                # Figure out the extension of the default value, if it has one.
+                ext = m.original[m.altered].to_s.split( '.' )
+                ext = ext.size > 1 ? ext.last : nil
+
+                # Null-terminate the injected value and append the ext.
+                m.altered_value += "\x00.#{ext}"
+
+                # Pass our new element back to be audited.
+                m
+            end
         }
     end
 
