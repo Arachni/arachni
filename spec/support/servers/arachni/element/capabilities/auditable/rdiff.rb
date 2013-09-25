5be382da4dcd06aeb1af7c830fb0612ad2f95991
Tasos Laskos <tasos.laskos@gmail.com>
2013-09-25 21:24:07 +0300

    Auditable::RDiff: Updated the algorithm to be more lax
    Also:
    
        * Added integrity check for the analysis process.
        * Optimized scheduling of data gathering.
        * Reduced total amount of performed requests.

--- spec/support/servers/arachni/element/capabilities/auditable/rdiff.rb
+++ spec/support/servers/arachni/element/capabilities/auditable/rdiff.rb
@@ -13,11 +13,10 @@ get '/true' do
     end
 
     <<-EOHTML
-    #{rand(9999999)}
+    #{rand( 9999999 )}
     <a href='?rdiff=blah'>Inject here</a>
     #{out}
 EOHTML
-
 end
 
 get '/false' do
@@ -31,8 +30,47 @@ get '/false' do
     end
 
     <<-EOHTML
-    #{rand(9999999)}
+    #{rand( 9999999 )}
     <a href='?rdiff=blah'>Inject here</a>
     #{out}
 EOHTML
+end
+
+get '/timeout' do
+    out = case params[:rdiff]
+              when 'blahbad'
+                  'Could not find any results, bugger off!'
+              when 'blahgood', 'blah'
+                  sleep 2
+                  '1 item found: Blah blah blah...'
+              else
+                  'No idea what you want mate...'
+          end
+
+    <<-EOHTML
+    #{rand( 9999999 )}
+    <a href='?rdiff=blah'>Inject here</a>
+    #{out}
+    EOHTML
+end
+
+get '/empty' do
+    empty = false
+    out = case params[:rdiff]
+              when 'blahbad'
+                  'Could not find any results, bugger off!'
+              when 'blahgood', 'blah'
+                  '1 item found: Blah blah blah...'
+              else
+                  empty = true
+                  'No idea what you want mate...'
+          end
+
+    next '' if empty
+
+    <<-EOHTML
+    #{rand( 9999999 )}
+    <a href='?rdiff=blah'>Inject here</a>
+    #{out}
+    EOHTML
 end
