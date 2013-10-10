cf0e600976b0d2c906acba0c851d9585c2bde17a
Tasos Laskos <tasos.laskos@gmail.com>
2013-10-11 01:02:13 +0300

    Support::Database::Queue: Small code cleanup

--- lib/arachni/support/database/queue.rb
+++ lib/arachni/support/database/queue.rb
@@ -17,83 +17,66 @@
 module Arachni
 module Support::Database
 
-    #
-    # Flat-file Queue implementation
-    #
-    # Behaves pretty much like a Ruby Queue however it transparently serializes and
-    # saves its values to the file-system under the OS's temp directory.
-    #
-    # It's pretty useful when you want to reduce memory footprint without
-    # having to refactor any code since it behaves just like Ruby's implementation.
-    #
-    # @author Tasos "Zapotek" Laskos
-    #                                      <tasos.laskos@gmail.com>
-    #                                      
-    # @version 0.1
-    #
-    class Queue < Base
+# Flat-file Queue implementation
+#
+# Behaves pretty much like a Ruby Queue however it transparently serializes and
+# saves its values to the file-system under the OS's temp directory.
+#
+# It's pretty useful when you want to reduce memory footprint without
+# having to refactor any code since it behaves just like Ruby's implementation.
+#
+# @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
+class Queue < Base
 
-        #
-        # @see Arachni::Database::Base#initialize
-        #
-        def initialize( *args )
-            super( *args )
-            @q = ::Queue.new
-        end
-
-        #
-        # Adds an object to the queue.
-        #
-        # @param    [Object]    obj
-        #
-        def <<( obj )
-            @q << dump( obj )
-        end
-        alias :push :<<
-        alias :enq :<<
-
-        #
-        # Removes an object from the Queue and returns it.
-        #
-        # @return   [Object]
-        #
-        def pop
-            return load_and_delete_file( @q.pop )
-        end
-        alias :deq :pop
-        alias :shift :pop
-
-        #
-        # Size of the Queue, the number of objects it currently holds.
-        #
-        # @return   [Integer]
-        #
-        def size
-            @q.size
-        end
-        alias :length :size
-
-        #
-        # True if the Queue if empty, false otherwise.
-        #
-        # @return   [Bool]
-        #
-        def empty?
-            @q.empty?
-        end
-
-        #
-        # Removes all objects from the Queue.
-        #
-        def clear
-            while( !@q.empty? )
-                path = @q.pop
-                next if !path
-                delete_file( path )
-            end
-        end
-
+    # @see Arachni::Database::Base#initialize
+    def initialize( *args )
+        super( *args )
+        @q     = ::Queue.new
+        @mutex = Mutex.new
     end
+
+    # @param    [Object]    obj Object to add to the queue.
+    def <<( obj )
+        synchronize { @q << dump( obj ) }
+    end
+    alias :push :<<
+    alias :enq :<<
+
+    # @return   [Object] Removes an object from the queue and returns it.
+    def pop
+        synchronize { load_and_delete_file @q.pop }
+    end
+    alias :deq :pop
+    alias :shift :pop
+
+    # @return   [Integer]
+    #   Size of the queue, the number of objects it currently holds.
+    def size
+        @q.size
+    end
+    alias :length :size
+
+    # @return   [Bool] `true` if the queue if empty, `false` otherwise.
+    def empty?
+        @q.empty?
+    end
+
+    # Removes all objects from the queue.
+    def clear
+        while !@q.empty?
+            path = @q.pop
+            next if !path
+            delete_file path
+        end
+    end
+
+    private
+
+    def synchronize( &block )
+        @mutex.synchronize( &block )
+    end
+
+end
 
 end
 end
