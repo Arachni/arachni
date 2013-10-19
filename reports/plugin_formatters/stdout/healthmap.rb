4f6c5b174509a238a78d1423df1eb0402242f3fc
Tasos Laskos <tasos.laskos@gmail.com>
2013-10-20 01:50:37 +0300

    healthmap plugin stdout formatter: Sort by URL

--- reports/plugin_formatters/stdout/healthmap.rb
+++ reports/plugin_formatters/stdout/healthmap.rb
@@ -16,11 +16,9 @@
 
 class Arachni::Reports::Stdout
 
-#
 # Stdout formatter for the results of the HealthMap plugin
 #
 # @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
-#
 class PluginFormatters::HealthMap < Arachni::Plugin::Formatter
 
     def run
@@ -29,7 +27,7 @@ class PluginFormatters::HealthMap < Arachni::Plugin::Formatter
         print_bad 'Has issues'
         print_line
 
-        results[:map].each do |i|
+        results[:map].sort_by { |_, v| v }.each do |i|
             state = i.keys[0]
             url   = i.values[0]
 
