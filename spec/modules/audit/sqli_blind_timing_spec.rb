92377f1ad4592f6ad2e19fc02f82d2924cefdd80
Tasos Laskos <tasos.laskos@gmail.com>
2013-10-22 16:16:09 +0300

    sqli_blind_timing_spec: Updated expectations

--- spec/modules/audit/sqli_blind_timing_spec.rb
+++ spec/modules/audit/sqli_blind_timing_spec.rb
@@ -13,8 +13,8 @@ describe name_from_filename do
 
     def issue_count_per_target
         {
-            mysql:      60,
-            postgresql: 28,
+            mysql:      16,
+            postgresql: 6,
             mssql:      9
         }
     end
