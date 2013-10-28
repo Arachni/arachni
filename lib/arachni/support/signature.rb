9aa6963bea76a24d779fc99f28a1595aa51bf08e
Tasos Laskos <tasos.laskos@gmail.com>
2013-10-28 20:44:03 +0200

    Support::Signature: Added threshold support for comparisons

--- lib/arachni/support/signature.rb
+++ lib/arachni/support/signature.rb
@@ -5,32 +5,68 @@
 
 module Arachni::Support
 
-# Represents a signature, used to keep a lightweight representation of a {String}
-# and refine it with more similar {String}s to remove noise.
+# Represents a signature, used to maintain a lightweight representation of a
+# {String} and refine it using similar {String}s to remove noise.
 #
 # @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
 class Signature
 
+    attr_reader :tokens
+
     # @note The string will be tokenized based on whitespace.
-    # @param    [#to_s]    seed    Initial seed for the signature.
-    def initialize( data )
-        @tokens = data.to_s.tokens
+    # @param    [String, Signature]    data
+    #   Seed data to use to initialize the signature.
+    # @param    [Hash]    options
+    # @option   options :threshold  [Integer]
+    #   Sets the maximum allowed difference (in tokens) when performing
+    #   {#== comparisons}.
+    def initialize( data, options = {} )
+        @tokens  = data.tokens
+        @options = options
+
+        if @options[:threshold] && !@options[:threshold].is_a?( Numeric )
+            fail ArgumentError, 'Option :threshold must be a number.'
+        end
     end
 
     # @note The string will be tokenized based on whitespace.
-    # @param    [#to_s]    data    Data to use to refine the signature.
+    # @param    [String, Signature]    data    Data to use to refine the signature.
     # @return   [Signature] `self`
-    def refine( data )
-        @tokens &= data.to_s.tokens
+    def refine!( data )
+        @tokens &= data.tokens
         self
     end
 
-    def hash
-        @tokens.hash
+    # @note The string will be tokenized based on whitespace.
+    # @param    [String, Signature]    data    Data to use to refine the signature.
+    # @return   [Signature] New, refined signature.
+    def refine( data )
+        dup.refine!( data )
     end
 
+    # @return [Signature]   Copy of `self`.
+    def dup
+        self.class.new( '' ).tap { |s| s.copy( tokens, @options ) }
+    end
+
+    def hash
+        tokens.hash
+    end
+
+    # @note Takes into account the `:threshold` {#initialize option}.
+    # @param [Signature]    other
     def ==( other )
-        hash == other.hash
+        return true  if hash == other.hash
+        return false if !@options[:threshold]
+
+        ((other.tokens - tokens) - (tokens - other.tokens)).size < @options[:threshold]
+    end
+
+    protected
+
+    def copy( tokens, options )
+        @tokens  = tokens.dup
+        @options = options.dup
     end
 
 end
