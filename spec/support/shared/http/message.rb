904b79f44a604ff839c6c1d156e92185791d299d
Tasos Laskos <tasos.laskos@gmail.com>
2013-12-03 03:59:54 +0200

    specs: Cleaned up http/message shared example

--- spec/support/shared/http/message.rb
+++ spec/support/shared/http/message.rb
@@ -1,44 +1,44 @@
 shared_examples_for 'Arachni::HTTP::Message' do
 
-  let(:url) { 'http://test.com' }
+    let(:url) { 'http://test.com' }
 
-  describe '#initialize' do
-    it 'sets the instance attributes by the options' do
-      options = {
-          version: '1.0',
-          headers: {
-              'X-Stuff' => 'Blah'
-          }
-      }
-      r = described_class.new( url, options )
-      r.version.should == options[:version]
-      r.headers.should == options[:headers]
-    end
-  end
-
-  describe '#version' do
-    it 'defaults to 1.1' do
-      described_class.new( url ).version.should == '1.1'
-    end
-  end
-
-  describe '#headers' do
-    context 'when not configured' do
-      it 'defaults to an empty Hash' do
-        described_class.new( url ).headers.should == {}
-      end
+    describe '#initialize' do
+        it 'sets the instance attributes by the options' do
+            options = {
+                version: '1.0',
+                headers: {
+                    'X-Stuff' => 'Blah'
+                }
+            }
+            r = described_class.new(url, options)
+            r.version.should == options[:version]
+            r.headers.should == options[:headers]
+        end
     end
 
-    it 'returns the configured value' do
-      headers = { 'Content-Type' => 'text/plain' }
-      described_class.new( url, headers: headers ).headers.should == headers
+    describe '#version' do
+        it 'defaults to 1.1' do
+            described_class.new(url).version.should == '1.1'
+        end
     end
-  end
 
-  describe '#body' do
-    it 'returns the configured body' do
-      body = 'Stuff...'
-      described_class.new( url, body: body ).body.should == body
+    describe '#headers' do
+        context 'when not configured' do
+            it 'defaults to an empty Hash' do
+                described_class.new(url).headers.should == {}
+            end
+        end
+
+        it 'returns the configured value' do
+            headers = { 'Content-Type' => 'text/plain' }
+            described_class.new(url, headers: headers).headers.should == headers
+        end
     end
-  end
+
+    describe '#body' do
+        it 'returns the configured body' do
+            body = 'Stuff...'
+            described_class.new(url, body: body).body.should == body
+        end
+    end
 end
