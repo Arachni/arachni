73df8a4794101d55f33c87fc1022095ab5461aaa
Tasos Laskos <tasos.laskos@gmail.com>
2014-02-03 04:39:23 +0200

    Watir::Element#opening_tag: Updated regexp to be multiline

--- lib/arachni/watir/element.rb
+++ lib/arachni/watir/element.rb
@@ -4,35 +4,35 @@
 =end
 
 module Watir
-    class Element
+class Element
 
-        def events
-            (browser.execute_script( 'return arguments[0].events;', self ) || []).
-                    map { |event, fn| [event.to_sym, fn] } |
-            (::Arachni::Browser.events.flatten.map(&:to_s) & attributes).
-                map { |event| [event.to_sym, attribute_value( event )] }
-        end
-
-        def submit
-            @element.submit
-        end
-
-        def opening_tag
-            html.match( /<#{tag_name}.*?>/i )[0]
-        end
-
-        def attributes
-            browser.execute_script(
-                %Q[
-                    var s = [];
-                    var attrs = arguments[0].attributes;
-                    for( var l = 0; l < attrs.length; ++l ) {
-                        s.push( attrs[l].name );
-                    }
-                    return s;
-                ],
-                self
-            )
-        end
+    def events
+        (browser.execute_script( 'return arguments[0].events;', self ) || []).
+                map { |event, fn| [event.to_sym, fn] } |
+        (::Arachni::Browser.events.flatten.map(&:to_s) & attributes).
+            map { |event| [event.to_sym, attribute_value( event )] }
     end
+
+    def submit
+        @element.submit
+    end
+
+    def opening_tag
+        html.match( /<#{tag_name}.*?>/im )[0]
+    end
+
+    def attributes
+        browser.execute_script(
+            %Q[
+                var s = [];
+                var attrs = arguments[0].attributes;
+                for( var l = 0; l < attrs.length; ++l ) {
+                    s.push( attrs[l].name );
+                }
+                return s;
+            ],
+            self
+        )
+    end
+end
 end
