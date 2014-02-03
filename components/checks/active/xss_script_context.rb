c6a788d1ba3ffd4c1d9e93f71f69aabb81966f70
Tasos Laskos <tasos.laskos@gmail.com>
2014-02-03 02:32:18 +0200

    xss_script_context: Optimized the rudimentary taint check

--- components/checks/active/xss_script_context.rb
+++ components/checks/active/xss_script_context.rb
@@ -15,6 +15,29 @@
 # @see http://secunia.com/advisories/9716/
 class Arachni::Checks::XssScriptContext < Arachni::Check::Base
 
+    ATTRIBUTES = [
+        'onload',
+        'onunload',
+        'onblur',
+        'onchange',
+        'onfocus',
+        'onreset',
+        'onselect',
+        'onsubmit',
+        'onabort',
+        'onkeydown',
+        'onkeypress',
+        'onkeyup',
+        'onclick',
+        'ondblclick',
+        'onmousedown',
+        'onmousemove',
+        'onmouseout',
+        'onmouseover',
+        'onmouseup',
+        'src'
+    ]
+
     def self.seed
         'window.top._%s_taint_tracer.log_execution_flow_sink()'
     end
@@ -46,8 +69,7 @@ class Arachni::Checks::XssScriptContext < Arachni::Check::Base
 
     def check_and_log( response, element )
         # Check to see if the response is tainted before going any further.
-        return if element.seed.to_s.empty? || !response.body ||
-            !response.body.include?( element.seed )
+        return if !tainted?( response, element.seed )
 
         print_info 'Response is tainted, scheduling a taint-trace.'
 
@@ -61,6 +83,21 @@ class Arachni::Checks::XssScriptContext < Arachni::Check::Base
 
             log( { vector: element, proof: element.seed }, page )
         end
+    end
+
+    def tainted?( response, seed )
+        return if seed.to_s.empty? || !response.body.to_s.include?( seed )
+
+        doc = Nokogiri::HTML( response.body )
+        return true if doc.css('script').to_s.include?( seed )
+
+        ATTRIBUTES.each do |attribute|
+            doc.xpath( "//*[@#{attribute}]" ).each do |elem|
+                return true if elem.attributes[attribute].to_s.include?( seed )
+            end
+        end
+
+        false
     end
 
     def self.info
