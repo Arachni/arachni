cb18f8550600e8d055840a0f15e785b2a71c24df
Tasos Laskos <tasos.laskos@gmail.com>
2014-02-06 02:27:12 +0200

    trainer check: Updated to fingerprint responses

--- components/checks/active/trainer.rb
+++ components/checks/active/trainer.rb
@@ -3,12 +3,13 @@
     All rights reserved.
 =end
 
-# Pokes and probes all inputs of a given page in order to uncover new input vectors.
+# Pokes and probes all inputs of a given page in order to uncover new input
+# vectors and performs platforms fingerprinting of responses.
 #
 # It also forces Arachni to train itself by analyzing the server responses.
 #
 # @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
-# @version 0.1.2
+# @version 0.1.3
 class Arachni::Checks::Trainer < Arachni::Check::Base
 
     def run
@@ -17,7 +18,11 @@ class Arachni::Checks::Trainer < Arachni::Check::Base
         # wasting bandwidth.
         return if framework.page_limit_reached?
 
-        audit( "_arachni_trainer_#{seed}", train: true, param_flip: true ){}
+        audit( "_arachni_trainer_#{seed}", train: true, param_flip: true ) do |response, _|
+            # Forces the response to be fingerprinted as all pages automatically
+            # get fingerprinted if that option has been enabled.
+            response.to_page
+        end
     end
 
     def self.info
@@ -27,7 +32,7 @@ class Arachni::Checks::Trainer < Arachni::Check::Base
                 It also forces Arachni to train itself by analyzing the server responses.},
             author:      'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>',
             elements:    [ Element::Form, Element::Link, Element::Cookie, Element::Header ],
-            version:     '0.1.2',
+            version:     '0.1.3',
             targets:     %w(Generic)
         }
     end
