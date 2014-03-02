504d26b7c1bfad11e07697416b1f69b713e29ab1
Tasos Laskos <tasos.laskos@gmail.com>
2014-03-02 21:13:15 +0200

    Support::Database::Queue spec: Rewrote it

--- spec/arachni/support/database/queue_spec.rb
+++ spec/arachni/support/database/queue_spec.rb
@@ -2,63 +2,95 @@ require 'spec_helper'
 
 describe Arachni::Support::Database::Queue do
 
-    before :all do
-        @seeds = [
-            'val',
-            :val,
-            { 'val' => :val },
-            [ 'val', :val ]
-        ]
-
-        @queue  = described_class.new
-        @seed_q = Queue.new
+    before :each do
+        @queue = described_class.new
     end
-
-    # http://www.ruby-doc.org/stdlib-1.9.3/libdoc/thread/rdoc/Queue.html#method-i-empty-3F
-    it 'implements #empty?' do
-        @queue.empty?.should == @seed_q.empty?
-    end
-
-    # http://www.ruby-doc.org/stdlib-1.9.3/libdoc/thread/rdoc/Queue.html#method-i-3C-3C
-    it 'implements #<<(v) (and #push(v), #enq(v))' do
-        @queue  << @seeds[0].should == @seed_q << @seeds[0]
-        @queue.push( @seeds[1] ).should == @seed_q.push( @seeds[1] )
-        @queue.enq( @seeds[2] ).should == @seed_q.enq( @seeds[2] )
-    end
-
-    # http://www.ruby-doc.org/stdlib-1.9.3/libdoc/thread/rdoc/Queue.html#method-i-pop
-    it 'implements #shift (and #pop, #deq)' do
-        @queue.pop.should == @seed_q.pop
-        @queue.shift.should == @seed_q.shift
-        @queue.deq.should == @seed_q.deq
-    end
-
-    # http://www.ruby-doc.org/stdlib-1.9.3/libdoc/thread/rdoc/Queue.html#method-i-size
-    it 'implements #size' do
-        @queue.size.should == @seed_q.size
-    end
-
-    # http://www.ruby-doc.org/stdlib-1.9.3/libdoc/thread/rdoc/Queue.html#method-i-clear
-    it 'implements #clear' do
+    subject { @queue }
+    after :each do
         @queue.clear
-        @seed_q.clear
-        @queue.size.should == @seed_q.size
+    end
+
+    describe '#empty?' do
+        context 'when the queue is empty' do
+            it 'returns true' do
+                subject.empty?.should be_true
+            end
+        end
+
+        context 'when the queue is not empty' do
+            it 'returns false' do
+                subject << :stuff
+                subject.empty?.should be_false
+            end
+        end
+    end
+
+    describe '#<<' do
+        it 'pushes an object' do
+            subject << :stuff
+            subject.pop.should == :stuff
+        end
+    end
+
+    describe '#push' do
+        it 'pushes an object' do
+            subject.push :stuff
+            subject.pop.should == :stuff
+        end
+    end
+
+    describe '#enq' do
+        it 'pushes an object' do
+            subject.enq :stuff
+            subject.pop.should == :stuff
+        end
     end
 
     describe '#pop' do
+        it 'removes an object' do
+            subject << :stuff
+            subject.pop.should == :stuff
+        end
+
         it 'blocks until an entry is available' do
             val = nil
 
-            t = Thread.new { val = @queue.pop }
+            t = Thread.new { val = subject.pop }
             sleep 1
-            Thread.new { @queue << :stuff }
+            Thread.new { subject << :stuff }
             t.join
 
             val.should == :stuff
         end
     end
 
-    after :all do
-        @queue.clear
+    describe '#deq' do
+        it 'removes an object' do
+            subject << :stuff
+            subject.deq.should == :stuff
+        end
     end
+
+    describe '#shift' do
+        it 'removes an object' do
+            subject << :stuff
+            subject.shift.should == :stuff
+        end
+    end
+
+    describe '#size' do
+        it 'returns the size of the queue' do
+            10.times { |i| subject << i }
+            subject.size.should == 10
+        end
+    end
+
+    describe '#clear' do
+        it 'empties the queue' do
+            10.times { |i| subject << i }
+            subject.clear
+            subject.size.should == 0
+        end
+    end
+
 end
