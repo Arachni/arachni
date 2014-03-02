e1e26852bebefd8c272132b331a65317aec22fc5
Treadie <ssgtreadaway@outlook.com>
2014-03-02 17:11:41 +1100

    Mod htaccessLimit: Updated description & remedy

--- modules/recon/htaccess_limit.rb
+++ modules/recon/htaccess_limit.rb
@@ -17,7 +17,7 @@
 #
 # @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
 #
-# @version 0.1.5
+# @version 0.1.6
 #
 class Arachni::Modules::Htaccess < Arachni::Module::Base
 
@@ -42,18 +42,40 @@ class Arachni::Modules::Htaccess < Arachni::Module::Base
                 GET requests but allows POST.},
             elements:    [ Element::SERVER ],
             author:      'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>',
-            version:     '0.1.5',
+            version:     '0.1.6',
             targets:     %w(Generic),
             references: {
                 'Apache.org' => 'http://httpd.apache.org/docs/2.2/mod/core.html#limit'
             },
             issue:       {
                 name:        %q{Misconfiguration in LIMIT directive of .htaccess file},
-                description: %q{The .htaccess file blocks GET requests but allows POST.},
+                description: %q{There are a number of HTTP methods that can be 
+                    used on a webserver, for example OPTIONS, HEAD, GET, POST, 
+                    PUT, DELETE etc.  Each of these methods perform a different 
+                    function, and each have an associate level of risk when 
+                    their use is permitted on the webserver. The <Limit> 
+                    directive within Apaches .htaccess file allows 
+                    administrators to define which of the methods they would 
+                    like to block. However as this is a blacklisting approach it 
+                    is inevitable that a server administrator may accidently 
+                    miss adding certain HTTP methods to be blocked, therefor 
+                    increasing the level of risk to the application and/or 
+                    server. For example the <Limit> directive may prevent PUT 
+                    however will permit put (in lower case). Arachni discovered 
+                    through method tampering that methods were able to be used 
+                    in both upper and lower case. Therefor indicating the use of 
+                    the less secure <Limit> directive.},
                 tags:        %w(htaccess server limit),
                 severity:    Severity::HIGH,
-                remedy_guidance:  %q{Do not use the LIMIT tag.
-                    If you are in a situation where you want to allow specific request methods, you should use LimitExcept instead.}
+                remedy_guidance:  %q{The preferred configuration to prevent the 
+                    use of unauthorised HTTP methods is to utilise the 
+                    <LimitExcept> directive. This directive uses a whitelisting 
+                    approach to permit HTTP methods while blocking all others 
+                    not listed in the directive, and will therefor block any 
+                    method tampering attempts. Most commonly, the only HTTP 
+                    methods required for most scenarios are GET, and POST. An 
+                    example of permitting these HTTP methods is: 
+                    '<LimitExcept POST GET> require valid-user </LimitExcept>'}
             }
         }
     end
