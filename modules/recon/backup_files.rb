ad9863079cd3a80f40b4695a84deedd98b68f8e8
Tasos Laskos <tasos.laskos@gmail.com>
2014-03-03 23:08:07 +0200

    nackup_files module: Updated info data

--- modules/recon/backup_files.rb
+++ modules/recon/backup_files.rb
@@ -14,17 +14,13 @@
     limitations under the License.
 =end
 
-#
 # Backup file discovery module.
 #
 # Appends common backup extentions to the filename of the page under audit<br/>
 # and checks for its existence.
 #
 # @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
-#
 # @version 0.2.3
-#
-#
 class Arachni::Modules::BackupFiles < Arachni::Module::Base
 
     def self.extensions
@@ -62,37 +58,37 @@ class Arachni::Modules::BackupFiles < Arachni::Module::Base
             version:     '0.2.3',
             targets:     %w(Generic),
             references: {
-                'WASC' => 'http://www.webappsec.org/projects/threat/classes/information_leakage.shtml',
-                'WASC' => 'http://projects.webappsec.org/w/page/13246953/Predictable%20Resource%20Location'
+                'WASC 1' => 'http://www.webappsec.org/projects/threat/classes/information_leakage.shtml',
+                'WASC 2' => 'http://projects.webappsec.org/w/page/13246953/Predictable%20Resource%20Location'
             },
             issue:       {
                 name:            %q{Backup file},
                 description:     %q{A common practice when administering web 
                     applications is to create a copy/backup of a particular file 
                     or directory prior to making any modification to the file. 
-                    Another common practice it to add an extension or change the 
-                    name to the original file representing it as a backup 
+                    Another common practice is to add an extension or change the
+                    name of the original file to signify that it is a backup
                     (examples include .bak, .orig, .backup, etc.). During the 
-                    initial recon stages of an attack cyber-criminals will 
+                    initial recon stages of an attack, cyber-criminals will
                     attempt to locate backup files by adding common extensions 
                     onto files already discovered on the webserver. By analysing 
                     the response headers from the server they are able to 
                     determine if the backup file exists. These backup files can 
                     then assist in further compromise of the web application. By 
                     utilising the same method, Arachni was able to discover a 
-                    possible backup file. },
+                    possible backup file.},
                 tags:            %w(path backup file discovery),
                 cew:             '530',
                 severity:        Severity::MEDIUM,
-                remedy_guidance: %q{Do not keep alternative versions of files 
-                    underneath the virtual web server root. When updating the 
+                remedy_guidance: %q{Do not keep obsolete versions of files
+                    under the virtual web server root. When updating the
                     site, delete or move the files to a directory outside the 
                     virtual root, edit them there, and move (or copy) the files 
                     back to the virtual root. Make sure that only the files that 
                     are actually in use reside under the virtual root. 
                     Preventing access without authentication may also be an 
                     option and stop a client being able to view the contents of 
-                    a file however it is still likely that the filenames will be 
+                    a file, however it is still likely that the filenames will be
                     able to be discovered. Using obscure filenames is only 
                     implementing security through obscurity and is not a 
                     recommended option.}
