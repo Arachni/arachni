5f3ab679afc90dde7fc8b3963b05d4dafb0ff4e4
Tasos Laskos <tasos.laskos@gmail.com>
2014-03-03 23:32:02 +0200

    htaccess_limit module: Updated info data

--- modules/recon/htaccess_limit.rb
+++ modules/recon/htaccess_limit.rb
@@ -14,11 +14,8 @@
     limitations under the License.
 =end
 
-#
 # @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
-#
 # @version 0.1.6
-#
 class Arachni::Modules::Htaccess < Arachni::Module::Base
 
     def run
@@ -52,28 +49,24 @@ class Arachni::Modules::Htaccess < Arachni::Module::Base
                 description: %q{There are a number of HTTP methods that can be 
                     used on a webserver, for example OPTIONS, HEAD, GET, POST, 
                     PUT, DELETE etc.  Each of these methods perform a different 
-                    function, and each have an associate level of risk when 
-                    their use is permitted on the webserver. The <Limit> 
-                    directive within Apaches .htaccess file allows 
+                    function, and each has an associated level of risk when
+                    their use is permitted on the webserver. The '<Limit>'
+                    directive within Apache's '.htaccess' file allows
                     administrators to define which of the methods they would 
-                    like to block. However as this is a blacklisting approach it 
-                    is inevitable that a server administrator may accidently 
-                    miss adding certain HTTP methods to be blocked, therefor 
+                    like to block. However, as this is a blacklisting approach, it
+                    is inevitable that a server administrator may accidentally
+                    miss adding certain HTTP methods to be blocked, therefore
                     increasing the level of risk to the application and/or 
-                    server. For example the <Limit> directive may prevent PUT 
-                    however will permit put (in lower case). Arachni discovered 
-                    through method tampering that methods were able to be used 
-                    in both upper and lower case. Therefor indicating the use of 
-                    the less secure <Limit> directive.},
+                    server.},
                 tags:        %w(htaccess server limit),
                 severity:    Severity::HIGH,
-                remedy_guidance:  %q{The preferred configuration to prevent the 
-                    use of unauthorised HTTP methods is to utilise the 
+                remedy_guidance:  %q{The preferred configuration is to prevent
+                    the use of unauthorised HTTP methods by utilising the
                     <LimitExcept> directive. This directive uses a whitelisting 
                     approach to permit HTTP methods while blocking all others 
                     not listed in the directive, and will therefor block any 
                     method tampering attempts. Most commonly, the only HTTP 
-                    methods required for most scenarios are GET, and POST. An 
+                    methods required for most scenarios are GET and POST. An
                     example of permitting these HTTP methods is: 
                     '<LimitExcept POST GET> require valid-user </LimitExcept>'}
             }
