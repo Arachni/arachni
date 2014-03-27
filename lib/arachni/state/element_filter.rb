2757c0469d63dded47976b8271f97913922e03b5
Tasos Laskos <tasos.laskos@gmail.com>
2014-03-27 20:54:39 +0200

    State::ElementFilter: Comment indentation fix

--- lib/arachni/state/element_filter.rb
+++ lib/arachni/state/element_filter.rb
@@ -1,6 +1,6 @@
 =begin
-Copyright 2010-2014 Tasos Laskos <tasos.laskos@gmail.com>
-All rights reserved.
+    Copyright 2010-2014 Tasos Laskos <tasos.laskos@gmail.com>
+    All rights reserved.
 =end
 
 module Arachni
