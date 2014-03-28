e0bb5c58e174d07dca5da7453f313ac88a0d5b5f
Tasos Laskos <tasos.laskos@gmail.com>
2014-03-28 12:25:38 +0200

    State#clear: Added specs

--- spec/arachni/state_spec.rb
+++ spec/arachni/state_spec.rb
@@ -19,4 +19,25 @@ describe Arachni::State do
             subject.audit.should be_kind_of described_class::Audit
         end
     end
+
+    describe '#element_filter' do
+        it "returns an instance of #{described_class::ElementFilter}" do
+            subject.element_filter.should be_kind_of described_class::ElementFilter
+        end
+    end
+
+    describe '#framework' do
+        it "returns an instance of #{described_class::Framework}" do
+            subject.framework.should be_kind_of described_class::Framework
+        end
+    end
+
+    describe '#clear' do
+        %w(issues audit element_filter framework).each do |method|
+            it "clears ##{method}" do
+                subject.send(method).should receive(:clear)
+                subject.clear
+            end
+        end
+    end
 end
