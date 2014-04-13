ecb93b8bd00188aeb115d545cbd5fcc234177c79
Tasos Laskos <tasos.laskos@gmail.com>
2014-04-13 16:00:56 +0300

    email_notify: Updated options to the new format

--- components/plugins/email_notify.rb
+++ components/plugins/email_notify.rb
@@ -5,61 +5,43 @@
 
 require 'pony'
 
-#
 # Uses the Pony gem send a notification (and optionally report) at the end
 # of the scan over SMTP.
 #
 # @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
-#
-# @version 0.1.2
-#
+# @version 0.1.3
 class Arachni::Plugins::EmailNotify < Arachni::Plugin::Base
 
     def run
         wait_while_framework_running
-        @auditstore = framework.auditstore
     end
 
     def clean_up
-        issue_cnt = @auditstore.issues.size
-        time      = @auditstore.delta_time
-        url       = framework.opts.url
+        auditstore = framework.auditstore
 
         opts = {
-            subject:     "Scan for #{url} finished in #{time}",
-            body:        "Found #{issue_cnt} unique issues.",
-            to:          options['to'],
-            cc:          options['cc'],
-            bcc:         options['bcc'],
-            from:        options['from'],
+            subject:     "Scan for #{framework.opts.url} finished in #{auditstore.delta_time}",
+            body:        "Found #{auditstore.issues.size} unique issues.",
+            to:          options[:to],
+            cc:          options[:cc],
+            bcc:         options[:bcc],
+            from:        options[:from],
             via:         :smtp,
             via_options: {
-                address:              options['server_address'],
-                port:                 options['server_port'],
-                enable_starttls_auto: options['tls'],
-                user_name:            options['username'],
-                password:             options['password'],
-                authentication:       !options['authentication'].empty? ? options['authentication'].to_sym : nil,
-                domain:               "localhost.localdomain"
+                address:              options[:server_address],
+                port:                 options[:server_port],
+                enable_starttls_auto: options[:tls],
+                user_name:            options[:username],
+                password:             options[:password],
+                authentication:       !options[:authentication].empty? ? options[:authentication].to_sym : nil,
+                domain:               'localhost.localdomain'
             }
         }
 
-        if options['report'] != 'none'
-            report = framework.reports[ options['report'] ]
-
-            rep_opts = {}
-            report.info[:options].each do |opt|
-                rep_opts[opt.name] = opt.default if opt.default
-            end
-
-            rep_opts['outfile'] = 'scan_report.' + options['report']
-            report.new( @auditstore, rep_opts ).run
-
+        if options[:report] != 'none'
             opts[:attachments] = {
-                rep_opts['outfile'] => File.read( rep_opts['outfile'] )
+                "report.#{options[:report]}" => framework.report_as( options[:report] )
             }
-
-            FileUtils.rm( rep_opts['outfile'] )
         end
 
         print_status 'Sending the notification...'
@@ -71,22 +53,54 @@ class Arachni::Plugins::EmailNotify < Arachni::Plugin::Base
 
     def self.info
         {
-            name: 'E-mail notify',
+            name:        'E-mail notify',
             description: %q{Sends a notification (and optionally a report) over SMTP at the end of the scan.},
-            author: 'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>',
-            version: '0.1.2',
-            options: [
-                Options::String.new( 'to', [true, 'E-mail address of the receiver.'] ),
-                Options::String.new( 'cc', [false, 'E-mail address to which to send a carbon copy of the notification.'] ),
-                Options::String.new( 'bcc', [false, 'E-mail address for a blind carbon copy.'] ),
-                Options::String.new( 'from', [true, 'E-mail address of the sender.'] ),
-                Options::Address.new( 'server_address', [true, 'Address of the SMTP server to use.'] ),
-                Options::Port.new( 'server_port', [true, 'SMTP port.'] ),
-                Options::Bool.new( 'tls', [false, 'Use TLS/SSL?.'] ),
-                Options::String.new( 'username', [true, 'SMTP username.'] ),
-                Options::String.new( 'password', [true, 'SMTP password.'] ),
-                Options::String.new( 'authentication', [false, 'Authentication.', 'plain', ['plain', 'login', 'cram_md5', '']] ),
-                Options::Enum.new( 'report', [false, 'Report type to send as an attachment.', 'txt', ['txt', 'xml', 'html', 'json', 'yaml', 'marshal' 'none']] )
+            author:      'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>',
+            version:     '0.1.3',
+            options:     [
+                Options::String.new( :to,
+                    required:    true,
+                    description: 'E-mail address of the receiver.'
+                ),
+                Options::String.new( :cc,
+                    description: 'E-mail address to which to send a carbon copy of the notification.'
+                ),
+                Options::String.new( :bcc,
+                    description: 'E-mail address for a blind carbon copy.'
+                ),
+                Options::String.new( :from,
+                    required:    true,
+                    description: 'E-mail address of the sender.'
+                ),
+                Options::Address.new( :server_address,
+                    required:    true,
+                    description: 'Address of the SMTP server to use.'
+                ),
+                Options::Port.new( :server_port,
+                    required:    true,
+                    description: 'SMTP port.'
+                ),
+                Options::Bool.new( :tls,
+                    description: 'Use TLS/SSL?.'
+                ),
+                Options::String.new( :username,
+                    required:    true,
+                    description: 'SMTP username.'
+                ),
+                Options::String.new( :password,
+                    required:    true,
+                    description: 'SMTP password.'
+                ),
+                Options::Enum.new( :authentication,
+                    description:  'Authentication.',
+                    default:      'plain',
+                    valid_values: ['plain', 'login', 'cram_md5', '']
+                ),
+                Options::Enum.new( :report,
+                    description:  'Report type to send as an attachment.',
+                    default:      'txt',
+                    valid_values: ['txt', 'xml', 'html', 'json', 'yaml', 'marshal' 'none']
+                )
             ]
 
         }
