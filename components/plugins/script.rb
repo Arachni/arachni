2659352f07e632d238254136079fff28b49c8d7b
Tasos Laskos <tasos.laskos@gmail.com>
2014-04-13 17:25:32 +0300

    script plugin: Updated options to the new format

--- components/plugins/script.rb
+++ components/plugins/script.rb
@@ -3,38 +3,39 @@
     All rights reserved.
 =end
 
-#
 # Loads and runs an external Ruby script under the scope of a plugin,
 # used for debugging and general hackery.
 #
 # @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
-#
-# @version 0.1.1
-#
+# @version 0.1.2
 class Arachni::Plugins::Script < Arachni::Plugin::Base
 
     def run
-        if Arachni::Options.rpc_server_address
+        if Arachni::RPC.constants.include?( :Server ) &&
+            framework.is_a?( Arachni::RPC::Server::Framework )
             print_error 'Cannot be executed while running as an RPC server.'
             return
         end
 
-        print_status "Loading #{options['path']}"
-        eval( IO.read( options['path'] ) )
+        print_status "Loading #{options[:path]}"
+        eval IO.read( options[:path] )
         print_status 'Done!'
     end
 
     def self.info
         {
-            :name           => 'Script',
-            :description    => %q{Loads and runs an external Ruby script under the scope of a plugin,
+            name:        'Script',
+            description: %q{Loads and runs an external Ruby script under the scope of a plugin,
                 used for debugging and general hackery.
 
                 Will not work over RPC.},
-            :author         => 'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>',
-            :version        => '0.1.1',
-            :options        => [
-                Options::Path.new( 'path', [ true, 'Path to the script.' ] )
+            author:      'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>',
+            version:     '0.1.2',
+            options:     [
+                Options::Path.new( :path,
+                    required:    true,
+                    description: 'Path to the script.'
+                )
             ]
         }
     end
--- spec/components/plugins/script_spec.rb
+++ spec/components/plugins/script_spec.rb
@@ -4,9 +4,10 @@ describe name_from_filename do
     include_examples 'plugin'
 
     it 'executes a Ruby script under the scope of the running plugin' do
-        options.plugins[name_from_filename] = { 'path' => fixtures_path + '/script_plugin.rb' }
+        options.url = web_server_url_for(:framework)
+        options.plugins[component_name] = { 'path' => fixtures_path + '/script_plugin.rb' }
 
         run
-        results_for( name_from_filename ).should == 'I\'m a script!'
+        actual_results.should == 'I\'m a script!'
     end
 end
