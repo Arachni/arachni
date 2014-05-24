1d55a9299c70558fcda738bbbadf4db202f342fe
Tasos Laskos <tasos.laskos@gmail.com>
2014-05-24 07:30:06 +0300

    check shared rspec example: Allow tests with empty issues

--- spec/support/shared/check.rb
+++ spec/support/shared/check.rb
@@ -182,9 +182,8 @@ shared_examples_for 'check' do
         e = element_type.to_s
         e = e[0...-1] if element_type.to_s[-1] == 's'
 
-        if logs_issues
+        if issues.any?
             # make sure we ONLY got results for the requested element type
-            issues.should be_any
             issues.map { |i| i.vector.class.type }.uniq.should == [e.to_sym]
 
             if current_check.info[:issue]
