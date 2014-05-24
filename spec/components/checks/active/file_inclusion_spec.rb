8ee007598e51bdf3aae42799db5556a88aa83368
Tasos Laskos <tasos.laskos@gmail.com>
2014-05-24 06:36:19 +0300

    file_inclusion check: Added support for LinkTemplate

--- spec/components/checks/active/file_inclusion_spec.rb
+++ spec/components/checks/active/file_inclusion_spec.rb
@@ -8,40 +8,46 @@ describe name_from_filename do
     end
 
     def self.elements
-        [ Element::Form, Element::Link, Element::Cookie, Element::Header ]
+        [ Element::Form, Element::Link, Element::Cookie, Element::Header,
+          Element::LinkTemplate ]
     end
 
     def issue_count_per_element_per_platform
         {
             unix:    {
-                Element::Form   => 16,
-                Element::Link   => 16,
-                Element::Cookie => 16,
-                Element::Header => 8
+                Element::Form         => 16,
+                Element::Link         => 16,
+                Element::Cookie       => 16,
+                Element::Header       => 8,
+                Element::LinkTemplate => 8
             },
             windows: {
-                Element::Form   => 48,
-                Element::Link   => 48,
-                Element::Cookie => 48,
-                Element::Header => 24
+                Element::Form         => 96,
+                Element::Link         => 96,
+                Element::Cookie       => 96,
+                Element::Header       => 48,
+                Element::LinkTemplate => 48
             },
             tomcat:  {
-                Element::Form   => 4,
-                Element::Link   => 4,
-                Element::Cookie => 4,
-                Element::Header => 2
+                Element::Form         => 8,
+                Element::Link         => 8,
+                Element::Cookie       => 8,
+                Element::Header       => 4,
+                Element::LinkTemplate => 4
             },
             php:  {
-                Element::Form   => 68,
-                Element::Link   => 68,
-                Element::Cookie => 68,
-                Element::Header => 34
+                Element::Form         => 120,
+                Element::Link         => 120,
+                Element::Cookie       => 120,
+                Element::Header       => 60,
+                Element::LinkTemplate => 60
             },
             perl:  {
-                Element::Form   => 68,
-                Element::Link   => 68,
-                Element::Cookie => 68,
-                Element::Header => 34
+                Element::Form         => 120,
+                Element::Link         => 120,
+                Element::Cookie       => 120,
+                Element::Header       => 60,
+                Element::LinkTemplate => 60
             }
         }
     end
