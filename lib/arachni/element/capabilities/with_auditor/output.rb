837325f785b466ce94d237dbdfd944fb55ac5c93
Tasos Laskos <tasos.laskos@gmail.com>
2014-05-27 04:56:40 +0300

    Element::Capabilities::WithAuditor::Output: Added #print_verbose

--- lib/arachni/element/capabilities/with_auditor/output.rb
+++ lib/arachni/element/capabilities/with_auditor/output.rb
@@ -11,10 +11,10 @@ module WithAuditor
 module Output
     extend Forwardable
 
-    [ :debug?, :print_error, :print_status, :print_info, :print_line, :print_ok,
-        :print_bad, :print_debug, :print_debug_backtrace, :print_error_backtrace,
-        :print_debug_level_1, :print_debug_level_2, :print_debug_level_3,
-        :debug_level_1?, :debug_level_2?, :debug_level_3?
+    [ :debug?, :print_error, :print_status, :print_verbose, :print_info,
+      :print_line, :print_ok, :print_bad, :print_debug, :print_debug_backtrace,
+      :print_error_backtrace, :print_debug_level_1, :print_debug_level_2,
+      :print_debug_level_3, :debug_level_1?, :debug_level_2?, :debug_level_3?
     ].each do |method|
         def_delegator :auditor, :with_browser_cluster
         define_method method do |*args|
