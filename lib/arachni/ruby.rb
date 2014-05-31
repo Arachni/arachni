292096f73345756adb16dca9216b57bf036762a2
Tasos Laskos <tasos.laskos@gmail.com>
2014-05-31 16:17:06 +0300

    Object, Enumerable: Removed #realsize monkey-patch

--- lib/arachni/ruby.rb
+++ lib/arachni/ruby.rb
@@ -7,7 +7,6 @@ lib = Arachni::Options.paths.lib
 require lib + 'ruby/object'
 require lib + 'ruby/hash'
 require lib + 'ruby/set'
-require lib + 'ruby/enumerable'
 require lib + 'ruby/array'
 require lib + 'ruby/string'
 require lib + 'ruby/io'
--- lib/arachni/ruby/object.rb
+++ lib/arachni/ruby/object.rb
@@ -3,55 +3,20 @@
     All rights reserved.
 =end
 
-#
 # Overloads the {Object} class providing a {#deep_clone} method.
 #
 # @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
-#
 class Object
 
-    #
     # Deep-clones self using a Marshal dump-load.
     #
     # @return   [Object]    Duplicate of self.
-    #
     def deep_clone
         Marshal.load( Marshal.dump( self ) )
     end
 
     def to_rpc_data_or_self
         respond_to?( :to_rpc_data ) ? to_rpc_data : self
-    end
-
-    #
-    # Attempts to approximate the real size of self by summing up the size of
-    # all its instance variables' values and names.
-    #
-    # @param    [Bool]  invoke_size
-    #   Whether or not to include self's `size` in the return value.
-    #
-    # @return   [Integer]  Size of self.
-    #
-    def realsize( invoke_size = true )
-        return 1 if nil?
-
-        sz = 0
-        sz = size rescue 0 if invoke_size
-
-        ivs = instance_variables
-        return sz if ivs.empty?
-
-        ivs.reduce( sz ) do |s, iv|
-            v = instance_variable_get( iv )
-            s += begin
-                rs = v.realsize
-                rs > 0 ? rs : v.size
-            rescue => e
-                ap e
-                0
-            end
-            s += iv.size
-        end
     end
 
 end
