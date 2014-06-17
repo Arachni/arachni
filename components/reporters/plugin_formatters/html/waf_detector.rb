03ee514afe8aecff807fcef6202a8f8b71bdc715
Tasos Laskos <tasos.laskos@gmail.com>
2014-06-17 23:29:03 +0300

    waf_detector plugin: Updated HTML formatter

--- components/reporters/plugin_formatters/html/waf_detector.rb
+++ components/reporters/plugin_formatters/html/waf_detector.rb
@@ -5,12 +5,11 @@
 
 class Arachni::Reporters::HTML
 
-#
-# HTML formatter for the results of the WAF Detector plugin
+# HTML formatter for the results of the WAF Detector plugin.
 #
 # @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
-#
 class PluginFormatters::WAFDetector < Arachni::Plugin::Formatter
+    include TemplateUtilities
 
     def run
         ERB.new( tpl ).result( binding )
@@ -19,8 +18,48 @@ class PluginFormatters::WAFDetector < Arachni::Plugin::Formatter
     def tpl
         <<-HTML
             <h3>Result</h3>
-            <blockquote><%=results[:msg]%></blockquote>
+
+            <p class="alert alert-<%= message_type %>">
+                <b class="fa fa-<%= icon %>"></b>
+
+                <strong><%= status %></strong>:
+                <%= message %>
+            </p>
         HTML
+    end
+
+    def status
+        escapeHTML results['status'].capitalize
+    end
+
+    def message
+        escapeHTML results['message'].capitalize.gsub( '_', ' ' )
+    end
+
+    def icon
+        case results['status']
+            when 'found'
+                'check'
+
+            when 'not_found'
+                'times'
+
+            when 'inconclusive'
+                'question'
+        end
+    end
+
+    def message_type
+        case results['status']
+            when 'found'
+                'success'
+
+            when 'not_found'
+                'danger'
+
+            when 'inconclusive'
+                'warning'
+        end
     end
 
 end
