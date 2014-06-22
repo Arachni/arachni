23e94b3da60b9a2e9778097375e07ef125d14185
Tasos Laskos <tasos.laskos@gmail.com>
2014-06-22 21:44:47 +0300

    Reporter::Base#format_plugin_results: Cleaned up formatter handling

--- lib/arachni/plugin/formatter.rb
+++ lib/arachni/plugin/formatter.rb
@@ -9,17 +9,19 @@ module Plugin
 # Will be extended by plugin formatters which provide plugin data formatting
 # for the reports.
 #
-# Plugin formatters will be in turn ran by [Arachni::Report::Bas#format_plugin_results].
+# Plugin formatters will be in turn ran by {Arachni::Report::Bas#format_plugin_results}.
 #
 # @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
 class Formatter
     include UI::Output
 
+    attr_reader :parent
     attr_reader :report
     attr_reader :results
     attr_reader :description
 
-    def initialize( report, plugin_data )
+    def initialize( parent, report, plugin_data )
+        @parent       = parent
         @report       = report
         @results      = plugin_data[:results]
         @description  = plugin_data[:description]
