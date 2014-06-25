a2576905a7cd668dc457297518241f77007dc42e
Tasos Laskos <tasos.laskos@gmail.com>
2014-06-25 16:21:40 +0300

    Updated yaml and marshal reporter specs

--- spec/components/reporters/yaml_spec.rb
+++ spec/components/reporters/yaml_spec.rb
@@ -4,10 +4,10 @@ describe name_from_filename do
     include_examples 'reporter'
 
     test_with_full_report do
-        full_report.should == YAML.load( IO.read( outfile ) )
+        full_report.to_h.to_yaml.should == IO.read( outfile )
     end
 
     test_with_empty_report do
-        empty_report.should == YAML.load( IO.read( outfile ) )
+        empty_report.to_h.to_yaml.should == IO.read( outfile )
     end
 end
