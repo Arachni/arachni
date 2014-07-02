17294d72a46a57d3927c269b7a568164b3785823
Tasos Laskos <tasos.laskos@gmail.com>
2014-07-03 00:17:37 +0300

    Fixed SSL for spec servers

--- spec/support/servers/arachni/http/proxy_server_https.rb
+++ spec/support/servers/arachni/http/proxy_server_https.rb
@@ -3,26 +3,11 @@ require 'webrick'
 require 'webrick/https'
 require 'openssl'
 
-name = "/C=US/ST=SomeState/L=SomeCity/O=Organization/OU=Unit/CN=localhost"
-ca  = OpenSSL::X509::Name.parse( name )
-key = OpenSSL::PKey::RSA.new( 1024 )
-crt = OpenSSL::X509::Certificate.new
-
-crt.version = 2
-crt.serial  = 1
-crt.subject = ca
-crt.issuer  = ca
-crt.public_key = key.public_key
-crt.not_before = Time.now
-crt.not_after  = Time.now + 1 * 365 * 24 * 60 * 60 # 1 year
-
 options = {
     Port:            ARGV[1].to_i,
     Host:            ARGV[3],
     SSLEnable:       true,
     SSLVerifyClient: OpenSSL::SSL::VERIFY_NONE,
-    SSLCertificate:  crt,
-    SSLPrivateKey:   key,
     SSLCertName:     [["CN", WEBrick::Utils::getservername]],
 }
 
