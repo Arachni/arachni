9b1760dc7589651f9ed6185bff55775dbf797228
Tasos Laskos <tasos.laskos@gmail.com>
2014-08-12 00:38:32 +0300

    unvalidated_redirect check: Updated issue description and remedy_guidance

--- components/checks/active/unvalidated_redirect.rb
+++ components/checks/active/unvalidated_redirect.rb
@@ -58,23 +58,46 @@ class Arachni::Checks::UnvalidatedRedirect < Arachni::Check::Base
     def self.info
         {
             name:        'Unvalidated redirect',
-            description: %q{Injects URLs and checks the Location HTTP response header field
-                and/or browser URL to determine whether the attack was successful.},
+            description: %q{
+Injects URLs and checks the `Location` HTTP response header field and/or browser
+URL to determine whether the attack was successful.
+},
             elements:    [Element::Form, Element::Link, Element::Cookie, Element::Header],
             author:      'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>',
             version:     '0.2',
 
             issue:       {
                 name:            %q{Unvalidated redirect},
-                description:     %q{The web application redirects users to unvalidated URLs.},
+                description:     %q{
+Web applications occasionally use parameter values to store the address of the
+page to which the client will be redirected -- for example:
+`yoursite.com/page.asp?redirect=www.yoursite.com/404.asp`
+
+An unvalidated redirect occurs when the client is able to modify the affected
+parameter value in the request and thus control the location of the redirection.
+For example, the following URL `yoursite.com/page.asp?redirect=www.anothersite.com`
+will redirect to `anothersite.com`.
+
+Cyber-criminals will abuse these vulnerabilities in social engineering attacks
+to get users to unknowingly visit a malicious site hosted by the cyber-criminal.
+
+Arachni has discovered that the server does not validate the parameter value prior
+to redirecting the client to the injected value.
+},
                 references:  {
                     'OWASP Top 10 2010' => 'http://www.owasp.org/index.php/Top_10_2010-A10-Unvalidated_Redirects_and_Forwards'
                 },
                 tags:            %w(unvalidated redirect injection header location),
                 cwe:             819,
                 severity:        Severity::MEDIUM,
-                remedy_guidance: %q{Server side verification should be employed
-                    to ensure that the redirect destination is the one intended.}
+                remedy_guidance: %q{
+The application should ensure that the supplied value for a redirect is permitted.
+This can be achieved by performing whitelisting on the parameter value.
+
+The whitelist should contain a list of pages or sites that the application is
+permitted to redirect users to. If the supplied value does not match any value
+in the whitelist then the server should redirect to a standard error page.
+}
             }
         }
     end
