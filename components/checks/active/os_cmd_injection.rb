d594073e4a7528c015551d274f6ef110610d0374
Tasos Laskos <tasos.laskos@gmail.com>
2014-08-11 23:20:23 +0300

    os_cmd_injection check: Updated issue description and remedy_guidance

--- components/checks/active/os_cmd_injection.rb
+++ components/checks/active/os_cmd_injection.rb
@@ -60,7 +60,9 @@ class Arachni::Checks::OSCmdInjection < Arachni::Check::Base
     def self.info
         {
             name:        'OS command injection',
-            description: %q{Tries to find operating system command injections.},
+            description: %q{
+Tries to find Operating System command injections.
+},
             elements:    [ Element::Form, Element::Link, Element::Cookie,
                            Element::Header, Element::LinkTemplate ],
             author:      'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com> ',
@@ -69,16 +71,40 @@ class Arachni::Checks::OSCmdInjection < Arachni::Check::Base
 
             issue:       {
                 name:            %q{Operating system command injection},
-                description:     %q{The web application allows an attacker to
-    execute arbitrary OS commands.},
+                description:     %q{
+To perform specific actions from within a web application, it is occasionally
+required to run Operating System commands and have the output of these commands
+captured by the web application and returned to the client.
+
+OS command injection occurs when user supplied input is inserted into one of these
+commands without proper sanitisation and executed by the server.
+
+Cyber-criminals will abuse this weakness to perform their own arbitrary commands
+on the server. This can include everything from simple `ping` commands to map the
+internal network, to obtaining full control of the server.
+
+Arachni was able to inject specific Operating System commands and have the output
+from that command contained within the server response. This indicates that proper
+input sanitisation is not occurring.
+},
                 references:  {
-                    'OWASP' => 'http://www.owasp.org/index.php/OS_Command_Injection'
+                    'OWASP' => 'http://www.owasp.org/index.php/OS_Command_Injection',
+                    'WASC'  => 'http://projects.webappsec.org/w/page/13246950/OS%20Commanding'
                 },
-                tags:            %w(os command code injection regexp),
+                tags:            %w(os command code injection regexp error),
                 cwe:             78,
                 severity:        Severity::HIGH,
-                remedy_guidance: %q{User inputs must be validated and filtered
-    before being evaluated as OS level commands.}
+                remedy_guidance: %q{
+It is recommended that untrusted data is never used to form a command to be
+executed by the OS.
+
+To validate data, the application should ensure that the supplied value contains
+only the characters that are required to perform the required action.
+
+For example, where the form field expects an IP address, only numbers and periods
+should be accepted. Additionally, all control operators (`&`, `&&`, `|`, `||`, `$`, `\`, `#`)
+should be explicitly denied, and never accepted as valid input by the server.
+},
             }
         }
     end
