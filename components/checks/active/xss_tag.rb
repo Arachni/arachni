2d827c4af7233902b535dea13dad2e9ad7d792cf
Tasos Laskos <tasos.laskos@gmail.com>
2014-08-12 01:59:03 +0300

    xss_tag check: Updated issue description and remedy_guidance

--- components/checks/active/xss_tag.rb
+++ components/checks/active/xss_tag.rb
@@ -8,7 +8,7 @@
 #
 # @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
 #
-# @version 0.1.6
+# @version 0.1.7
 #
 # @see http://cwe.mitre.org/data/definitions/79.html
 # @see http://ha.ckers.org/xss.html
@@ -58,24 +58,79 @@ class Arachni::Checks::XSSHTMLTag < Arachni::Check::Base
             description: %q{Cross-Site Scripting in HTML tag.},
             elements:    [ Element::Form, Element::Link, Element::Cookie, Element::Header ],
             author:      'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com> ',
-            version:     '0.1.6',
+            version:     '0.1.7',
 
             issue:       {
                 name:            %q{Cross-Site Scripting (XSS) in HTML tag},
-                description:     %q{Unvalidated user input is being embedded in a HTML element.
-    This can lead to a Cross-Site Scripting vulnerability or a form of HTML manipulation.},
+                description:     %q{
+Client-side scripts are used extensively by modern web applications.
+They perform both simple functions (such as the formatting of text) up to full
+manipulation of client side data and operating system interaction.
+
+Unlike traditional Cross Site Scripting (XSS), where the client is able to inject
+scripts into a request and have the server return the script to the client, DOM
+XSS does not require that a request be sent to the server and may be abused entirely
+within the loaded page.
+
+This occurs when elements of the DOM (known as the sources) are able to be
+manipulated to contain untrusted data.
+The client-side scripts (known as the sinks) in the affected page use or execute
+the untrusted data in an unsafe way.
+
+A common attack used by cyber-criminals is to steal a client’s session token by
+injecting JavaScript, however DOM XSS vulnerabilities can also be abused to exploit
+clients.
+
+Arachni has discovered that it is possible to insert content directly into a HTML
+tag. For example `<INJECTION_HERE href=.......etc>` where `INJECTION_HERE`
+represents the location where the Arachni payload was detected.
+},
                 references:  {
                     'ha.ckers' => 'http://ha.ckers.org/xss.html',
-                    'Secunia'  => 'http://secunia.com/advisories/9716/'
+                    'Secunia'  => 'http://secunia.com/advisories/9716/',
+                    'WASC' => 'http://projects.webappsec.org/w/page/13246920/Cross%20Site%20Scripting'
                 },
 
                 tags:            %w(xss script tag regexp dom attribute injection),
                 cwe:             79,
                 severity:        Severity::HIGH,
-                remedy_guidance: 'User inputs must be validated and filtered
-    before being returned as part of the HTML code of a page.'
-            }
+                remedy_guidance: %q{
+To remedy XSS vulnerabilities, it is important to never use untrusted or unfiltered
+data within the code of a HTML page.
 
+Untrusted data can originate not only form the client but potentially a third
+party or previously uploaded file etc.
+
+Filtering of untrusted data typically involves converting special characters to
+their HTML entity encoding equivalent (however, other methods do exist, see references).
+These special characters include:
+
+* `&`
+* `<`
+* `>`
+* `"`
+* `'`
+* `/`
+
+An example of HTML entity encoding is converting a `<` to `&lt;`.
+
+Although it is possible to filter untrusted input, there are five locations
+within a HTML page where untrusted input (even if it has been filtered) should
+never be placed:
+
+1. Directly in a script.
+2. Inside an HTML comment.
+3. In an attribute name.
+4. In a tag name.
+5. Directly in CSS.
+
+Each of these locations have their own form of escaping and filtering.
+
+_Because many browsers attempt to implement XSS protection, any manual verification
+of this finding should be conducted utilising multiple different browsers and
+browser versions._
+}
+            }
         }
     end
 
