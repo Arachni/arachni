472a79b8bf830fde357c45e205ca4f50299d6260
Tasos Laskos <tasos.laskos@gmail.com>
2014-08-11 20:54:33 +0300

    csrf check: Updated description and remedy_guidance

--- components/checks/active/csrf.rb
+++ components/checks/active/csrf.rb
@@ -141,20 +141,52 @@ class Arachni::Checks::CSRF < Arachni::Check::Base
     def self.info
         {
             name:        'CSRF',
-            description: %q{It uses 2-pass rDiff analysis to determine
-                which forms affect business logic and audits them for CSRF.
-                It requires a logged-in user's cookie-jar.},
+            description: %q{
+It uses differential analysis to determine which forms affect business logic and
+checks them for lack of anti-CSRF tokens.
+
+(Works best with a valid session.)
+},
             elements:    [ Element::Form ],
             author:      'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com> ',
             version:     '0.3.3',
 
             issue:       {
                 name:            %q{Cross-Site Request Forgery},
-                description:     %q{The web application does not, or can not,
-     sufficiently verify whether a well-formed, valid, consistent
-     request was intentionally provided by the user who submitted the request.
-     This is due to a lack of secure anti-CSRF tokens to verify
-     the freshness of the submitted data.},
+                description:     %q{
+In the majority of today's web applications, clients are required to submit forms
+which can perform sensitive operations.
+
+An example of such a form being used would be when an administrator wishes to create
+a new user for the application.
+
+In the simplest version of the form, the administrator would fill-in:
+
+* Name
+* Password
+* Role (level of access)
+
+Continuing with this example, Cross Site Request Forgery (CSRF) would occur when
+the administrator is tricked into clicking on a link, which if logged into the
+application, would automatically submit the form without any further interaction.
+
+Cyber-criminals will look for sites where sensitive functions are performed in
+this manner and then craft malicious requests that will be used against clients
+via a social engineering attack.
+
+There are 3 things that are required for a CSRF attack to occur:
+
+1. The form must perform some sort of sensitive action.
+2. The victim (the administrator the example above) must have an active session.
+3. Most importantly, all parameter values must be **known** or **guessable**.
+
+Arachni discovered that all parameters within the form were known or predictable
+and therefore the form could be vulnerable to CSRF.
+
+_Manual verification may be required to check whether the submission will then
+perform a sensitive action, such as reset a password, modify user profiles, post
+content for a forum, etc._
+},
                 references:  {
                     'Wikipedia'    => 'http://en.wikipedia.org/wiki/Cross-site_request_forgery',
                     'OWASP'        => 'http://www.owasp.org/index.php/Cross-Site_Request_Forgery_(CSRF)',
@@ -163,9 +195,19 @@ class Arachni::Checks::CSRF < Arachni::Check::Base
                 tags:            %w(csrf rdiff form token),
                 cwe:             352,
                 severity:        Severity::HIGH,
-                remedy_guidance: %q{A unique token that guaranties freshness of submitted
-    data must be added to all web application elements that can affect
-    business logic.}
+                remedy_guidance: %q{
+Based on the risk (determined by manual verification) of whether the form submission
+performs a sensitive action, the addition of anti-CSRF tokens is highly advised.
+
+These tokens can be configured in such a way that each session generates a new
+anti-CSRF token or such that each individual request requires a new token.
+
+It is equally important that the server track and maintain the status of each
+token (in order to reject requests accompanied by invalid ones) and therefore
+prevent cyber-criminals from knowing, guessing or reusing them.
+
+_For examples of framework specific remediation, please refer to the references._
+}
             }
         }
     end
