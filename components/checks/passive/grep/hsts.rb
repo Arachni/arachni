f86875c4889ac30cadc72ea5c697569d151fd55c
Tasos Laskos <tasos.laskos@gmail.com>
2014-08-12 03:03:36 +0300

    hsts check: Updated issue description and remedy_guidance

--- components/checks/passive/grep/hsts.rb
+++ components/checks/passive/grep/hsts.rb
@@ -27,13 +27,43 @@ class Arachni::Checks::HTTPSStrictTransportSecurity < Arachni::Check::Base
 
             issue:       {
                 name:        %q{Missing 'Strict-Transport-Security' header},
-                description: %q{The web application uses HTTPS without specifying a 'Strict-Transport-Security' header.},
+                description: %q{
+The HTTP protocol by itself is clear text, meaning that any data that is
+transmitted via HTTP can be captured and the contents viewed. To keep data
+private, and prevent it from being intercepted HTTP is often tunnelled through
+either Secure Sockets Layer (SSL), or Transport Layer Security (TLS).
+When either of these encryption standards are used it is referred to as HTTPS.
+
+HTTP Strict Transport Security (HSTS) is an optional response header that can be
+configured on the server to instruct the browser to only communicate via HTTPS.
+This will be enforced by the browser even if the user requests a HTTP resource
+on the same server.
+
+Cyber-criminals will often attempt to compromise sensitive information passed
+from the client to the server using HTTP. This can be conducted via various
+different Man in The Middle (MiTM) attacks or through network packet captures.
+
+Arachni discovered that the affected application is using HTTPS however does not
+use the HSTS header.
+},
                 references:  {
                     'OWASP'     => 'https://www.owasp.org/index.php/HTTP_Strict_Transport_Security',
                     'Wikipedia' => 'http://en.wikipedia.org/wiki/HTTP_Strict_Transport_Security'
                 },
                 cwe:         200,
-                severity:    Severity::MEDIUM
+                severity:    Severity::MEDIUM,
+                remedy_guidance: %q{
+Depending on the framework being used the implementation methods will vary,
+however it is advised that the `Strict-Transport-Security` header be configured
+on the server.
+
+One of the options for this header is the `max-age` which is a representation in
+milliseconds determining the time in which the client’s browser will adhere to
+the header policy.
+
+Depending on the environment and the application this time period could be from
+as low as minutes to as long as days.
+}
             }
         }
     end
