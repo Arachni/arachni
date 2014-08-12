78a5da4b42254defce04093709b7aa20e547d3bf
Tasos Laskos <tasos.laskos@gmail.com>
2014-08-12 13:08:31 +0300

    localstart_asp check: Updated issue description and remedy_guidance

--- components/checks/passive/localstart_asp.rb
+++ components/checks/passive/localstart_asp.rb
@@ -4,7 +4,6 @@
 =end
 
 # @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
-# @version 0.1.1
 class Arachni::Checks::LocalstartASP < Arachni::Check::Base
 
     def run
@@ -38,14 +37,47 @@ class Arachni::Checks::LocalstartASP < Arachni::Check::Base
             description: %q{Checks for localstart.asp.},
             elements:    [ Element::Server ],
             author:      'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>',
-            version:     '0.1.1',
+            version:     '0.1.2',
 
             issue:       {
                 name:            %q{Exposed localstart.asp page},
-                description:     %q{The default management ISS page localstart.asp
-                    is still on the server.},
+                description:     %q{
+To restrict access to specific pages on a webserver, developers can implement
+various methods of authentication, therefore only allowing access to clients
+with valid credentials.
+There are several forms of authentication that can be used. The simplest forms of
+authentication are known as 'Basic' and 'Basic Realm'.
+These methods of authentication have several known weaknesses such as being
+susceptible to brute force attacks.
+
+Additionally, when utilising the NTLM mechanism in a windows environment, several
+disclosures of information exist, and any brute force attack occurs against the
+server's local users, or domain users if the web server is a domain member.
+
+Cyber-criminals will attempt to locate protected pages to gain access to them
+and also perform brute force attacks to discover valid credentials.
+
+Arachni discovered the following page requires NTLM based basic authentication
+in order to be accessed.
+},
                 tags:            %w(asp iis server),
-                severity:        Severity::LOW
+                severity:        Severity::LOW,
+                remedy_guidance: %q{
+If the pages being protected are not required for the functionality of the web
+application they should be removed, otherwise, it is recommended that basic
+and basic realm authentication are not used to protect against pages requiring
+authentication.
+
+If NTLM based basic authentication must be used, then default server and domain
+accounts such as `administrator` and `root` should be disabled, as these will
+undoubtedly be the first accounts to be targeted in any such attack.
+
+Additionally, the webserver should not be joined to any corporate domain where
+usernames are readily available (such as from email addresses). If the pages are
+required, and it is possible to remove the basic authentication, then a stronger
+and more resilient form-based authentication mechanism should be implemented to
+protect the affected pages.
+}
             }
         }
     end
