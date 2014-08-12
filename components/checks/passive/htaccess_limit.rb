42e7ca3aded85450dbe98de664cc4facf938c834
Tasos Laskos <tasos.laskos@gmail.com>
2014-08-12 12:58:47 +0300

    htaccess_limit check: Updated issue description and remedy_guidance

--- components/checks/passive/htaccess_limit.rb
+++ components/checks/passive/htaccess_limit.rb
@@ -4,7 +4,6 @@
 =end
 
 # @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
-# @version 0.1.5
 class Arachni::Checks::Htaccess < Arachni::Check::Base
 
     def run
@@ -29,18 +28,39 @@ class Arachni::Checks::Htaccess < Arachni::Check::Base
                 GET requests but allows POST.},
             elements:    [ Element::Server ],
             author:      'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>',
-            version:     '0.1.5',
+            version:     '0.1.6',
 
             issue:       {
                 name:        %q{Misconfiguration in LIMIT directive of .htaccess file},
-                description: %q{The .htaccess file blocks GET requests but allows POST.},
+                description: %q{
+There are a number of HTTP methods that can be used on a webserver (for example
+`OPTIONS`, `HEAD`, `GET`, `POST`, `PUT`, `DELETE `etc.).
+Each of these methods perform a different function, and each has an associated
+level of risk when their use is permitted on the webserver.
+
+The `<Limit>` directive within Apache's `.htaccess` file allows administrators
+to define which of the methods they would like to block. However, as this is a
+blacklisting approach, it is inevitable that a server administrator may
+accidentally miss adding certain HTTP methods to be blocked, thus increasing
+the level of risk to the application and/or server.
+},
                 references: {
                     'Apache.org' => 'http://httpd.apache.org/docs/2.2/mod/core.html#limit'
                 },
                 tags:        %w(htaccess server limit),
                 severity:    Severity::HIGH,
-                remedy_guidance:  %q{Do not use the LIMIT tag.
-                    If you are in a situation where you want to allow specific request methods, you should use LimitExcept instead.}
+                remedy_guidance:  %q{
+The preferred configuration is to prevent the use of unauthorised HTTP methods
+by utilising the `<LimitExcept>` directive.
+
+This directive uses a whitelisting approach to permit HTTP methods while
+blocking all others not listed in the directive, and will therefor block any
+method tampering attempts.
+
+Most commonly, the only HTTP methods required for most scenarios are `GET` and
+`POST`. An example of permitting these HTTP methods is:
+ `<LimitExcept POST GET> require valid-user </LimitExcept>`
+}
             }
         }
     end
