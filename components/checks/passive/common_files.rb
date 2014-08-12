5f2575fdf7569a39cafa614ad604c8530a52bd07
Tasos Laskos <tasos.laskos@gmail.com>
2014-08-12 12:50:02 +0300

    common_files check: Updated issue description and remedy_guidance

--- components/checks/passive/common_files.rb
+++ components/checks/passive/common_files.rb
@@ -6,7 +6,6 @@
 # Looks for sensitive common files on the server.
 #
 # @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
-# @version 0.2.2
 class Arachni::Checks::CommonFiles < Arachni::Check::Base
 
     def self.filenames
@@ -27,16 +26,43 @@ class Arachni::Checks::CommonFiles < Arachni::Check::Base
             description: %q{Tries to find common sensitive files on the server.},
             elements:    [ Element::Server ],
             author:      'Tasos "Zapotek" Laskos <tasos.laskos@gmail.com> ',
-            version:     '0.2.2',
+            version:     '0.2.3',
 
             issue:       {
                 name:            %q{Common sensitive file},
+                description:     %q{
+Web applications are often made up of multiple files and directories.
+
+It is possible that over time some files may become unreferenced (unused)
+by the web application and forgotten about by the administrator/developer.
+Because web applications are built using common frameworks, they contain common
+files that can be discovered (independent of server).
+
+During the initial recon stages of an attack, cyber-criminals will attempt to
+locate unreferenced files in the hope that the file will assist in further
+compromise of the web application.
+To achieve this they will make thousands of requests using word lists containing
+common filenames.
+The response headers from the server will then indicate if the file exists.
+
+Arachni also contains a list of common file names which it will attempt to access.
+},
                 references: {
                     'Apache.org' => 'http://httpd.apache.org/docs/2.0/mod/mod_access.html'
                 },
                 tags:            %w(common path file discovery),
                 severity:        Severity::LOW,
-                remedy_guidance: %q{Do not expose file and directory information to the user.}
+                remedy_guidance: %q{
+If files are unreferenced then they should be removed from the web root
+and/or the application directory.
+
+Preventing access without authentication may also be an option and can stop a
+client from being able to view the contents of a file, however it is still likely
+that the directory structure will be able to be discovered.
+
+Using obscure file names is implementing security through obscurity and is
+not a recommended option.
+}
             }
         }
     end
