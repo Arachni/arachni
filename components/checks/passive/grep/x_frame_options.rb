43d14a2ff03efdd9ea5ba18668426369c1154efc
Tasos Laskos <tasos.laskos@gmail.com>
2015-03-20 04:02:13 +0200

    x_frame_options check: Added data to .info

--- components/checks/passive/grep/x_frame_options.rb
+++ components/checks/passive/grep/x_frame_options.rb
@@ -29,13 +29,30 @@ class Arachni::Checks::XFrameOptions < Arachni::Check::Base
 
             issue:       {
                 name:        %q{Missing 'X-Frame-Options' header},
-                description: %q{},
+                description: %q{
+Clickjacking (User Interface redress attack, UI redress attack, UI redressing)
+is a malicious technique of tricking a Web user into clicking on something different
+from what the user perceives they are clicking on, thus potentially revealing
+confidential information or taking control of their computer while clicking on
+seemingly innocuous web pages.
+
+The server didn't return an `X-Frame-Options` header which means that this website
+could be at risk of a clickjacking attack.
+
+The `X-Frame-Options` HTTP response header can be used to indicate whether or not
+a browser should be allowed to render a page inside a frame or iframe. Sites can
+use this to avoid clickjacking attacks, by ensuring that their content is not
+embedded into other sites.
+},
                 references:  {
-                    'MDN' => 'https://developer.mozilla.org/en-US/docs/Web/HTTP/X-Frame-Options',
-                    'RFC' => 'http://tools.ietf.org/html/rfc7034'
+                    'MDN'   => 'https://developer.mozilla.org/en-US/docs/Web/HTTP/X-Frame-Options',
+                    'RFC'   => 'http://tools.ietf.org/html/rfc7034',
+                    'OWASP' => 'https://www.owasp.org/index.php/Clickjacking'
                 },
                 severity:    Severity::LOW,
-                remedy_guidance: %q{}
+                remedy_guidance: %q{
+Configure your web server to include an X-Frame-Options header.
+}
             }
         }
     end
