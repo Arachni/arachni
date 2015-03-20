1aa85c532717b6d686509b328955cdc2cbe781b3
Tasos Laskos <tasos.laskos@gmail.com>
2015-03-20 03:46:37 +0200

    insecure_client_access_policy: Added .info data

--- components/checks/passive/insecure_client_access_policy.rb
+++ components/checks/passive/insecure_client_access_policy.rb
@@ -55,11 +55,34 @@ Checks `clientaccesspolicy.xml` files for wildcard domain policies.
             elements:    [ Element::Server ],
 
             issue:       {
-                name:        %q{Insecure client-access policy},
-                description: %q{},
-                references:  {},
-                severity:    Severity::LOW,
-                remedy_guidance: %q{}
+                name:            %q{Insecure client-access policy},
+                description:     %q{
+The browser security model normally prevents web content from one domain from
+accessing data from another domain. This is commonly known as the "same origin policy".
+
+URL policy files grant cross-domain permissions for reading data. They permit
+operations that are not permitted by default. The URL policy file for Silverlight
+is located, by default, in the root directory of the target server, with the name
+`ClientAccessPolicy.xml` (for example, at `www.example.com/ClientAccessPolicy.xml`).
+
+When a domain is specified in `ClientAccessPolicy.xml`, the site declares that it
+is willing to allow the operators of any servers in that domain to obtain any
+document on the server where the policy file resides.
+
+The `ClientAccessPolicy.xml` file deployed on this website opens the server to all
+domains (use of a single asterisk "*" as a pure wildcard is supported).
+},
+                references:      {
+                    'MSDN'  => 'https://msdn.microsoft.com/en-us/library/cc197955%28v=vs.95%29.aspx'
+                },
+                cwe:             16,
+                severity:        Severity::LOW,
+                remedy_guidance: %q{
+Carefully evaluate which sites will be allowed to make cross-domain calls.
+
+Consider network topology and any authentication mechanisms that will be affected
+by the configuration or implementation of the cross-domain policy.
+}
             }
         }
     end
