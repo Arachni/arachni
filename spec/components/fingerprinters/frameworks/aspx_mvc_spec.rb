a8658d40f18155cee39b4da2e3e64db84809a8c7
Tasos Laskos <tasos.laskos@gmail.com>
2015-05-15 09:28:18 +0300

    Added aspx_mvc spec

---
+++ spec/components/fingerprinters/frameworks/aspx_mvc_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe Arachni::Platform::Fingerprinters::ASPXMVC do
+    include_examples 'fingerprinter'
+
+    def check_platforms( page )
+        [:asp, :aspx, :windows, :aspx_mvc].each do |p|
+            platforms_for( page ).should include p
+        end
+    end
+
+    context 'when there is a __requestverificationtoken cookie' do
+        it 'identifies it as ASP.NET MVC' do
+            check_platforms Arachni::Page.from_data(
+                url:     'http://stuff.com/blah',
+                cookies: [Arachni::Cookie.new(
+                              url:    'http://stuff.com/blah',
+                              inputs: { '__requestverificationtoken' => 'stuff' } )]
+
+            )
+        end
+    end
+
+    context 'when there is an X-AspNetMvc-Version header' do
+        it 'identifies it as ASP.NET MVC' do
+            check_platforms Arachni::Page.from_data(
+                url: 'http://stuff.com/blah',
+                response: { headers: { 'X-AspNetMvc-Version' => '2.0' } }
+            )
+        end
+    end
+
+end
