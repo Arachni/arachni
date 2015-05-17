797c997796b5f7bcdd5c1c5ffc5c45ee77dd47ce
Tasos Laskos <tasos.laskos@gmail.com>
2015-05-17 13:23:51 +0300

    Ethon::Easy::Callbacks#debug_callback: Only log request traffic

--- lib/arachni/ethon/easy.rb
+++ lib/arachni/ethon/easy.rb
@@ -11,9 +11,12 @@ class Easy
 module Callbacks
     def debug_callback
         @debug_callback ||= proc do |handle, type, data, size, udata|
+            # We only care about these so that we can have access to raw
+            # HTTP request traffic for reporting/debugging purposes.
+            next if type != :header_out && type != :data_out
+
             message = data.read_string( size )
             @debug_info.add type, message
-            # print message unless [:data_in, :data_out].include?(type)
             0
         end
     end
