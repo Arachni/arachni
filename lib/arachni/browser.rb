043e9cdfab9d370154edcf4b954897aa5afa7cce
Tasos Laskos <tasos.laskos@gmail.com>
2015-11-22 10:00:20 +0200

    Browser#spawn_phantomjs: pgroup => new_group

--- lib/arachni/browser.rb
+++ lib/arachni/browser.rb
@@ -1221,7 +1221,7 @@ class Browser
                         executable: self.class.executable,
                         without_arachni: true,
                         fork: false,
-                        pgroup: true,
+                        new_pgroup: true,
                         stdin: ri,
                         stdout: w,
                         stderr: w,
