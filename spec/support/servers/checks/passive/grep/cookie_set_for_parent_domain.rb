42efe3db8d4716bd1ca450b6da966d6f260338ec
Tasos Laskos <tasos.laskos@gmail.com>
2016-04-24 18:47:59 +0300

    cookie_set_for_parent_domain check: Fixed server responses

--- spec/support/servers/checks/passive/grep/cookie_set_for_parent_domain.rb
+++ spec/support/servers/checks/passive/grep/cookie_set_for_parent_domain.rb
@@ -8,14 +8,12 @@ get '/' do
     })
     response.set_cookie( 'cookie2', {
         value:  'value2',
-        domain: '.localhost'
+        domain: 'localhost'
     })
     response.set_cookie( 'cookie3', {
-        value:  'value3',
-        domain: 'localhost'
+        value:  'value3'
     })
     response.set_cookie( 'cookie4', {
-        value:  'value4',
-        domain: 'localhost'
+        value:  'value4'
     })
 end
