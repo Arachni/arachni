dd5a9bfc39aa6aeccb5b2961be1f5c983154f9c4
Tasos Laskos <tasos.laskos@gmail.com>
2016-08-05 12:07:58 +0300

    Browser#selenium: Set Selenium timeout to HTTP request timeout

--- lib/arachni/browser.rb
+++ lib/arachni/browser.rb
@@ -1095,7 +1095,7 @@ class Browser
             client = Selenium::WebDriver::Remote::Http::Typhoeus.new
         end
 
-        client.timeout = Options.browser_cluster.job_timeout
+        client.timeout = Options.http.request_timeout / 1_000
 
         @selenium = Selenium::WebDriver.for(
             :remote,
