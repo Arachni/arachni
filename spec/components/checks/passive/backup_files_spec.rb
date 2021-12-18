9626e4e7a0234120ec401216f6703c2e21b9072f
Tasos Laskos <tasos.laskos@gmail.com>
2021-12-18 12:34:05 +0200

    backup_files spec: Updated

--- spec/components/checks/passive/backup_files_spec.rb
+++ spec/components/checks/passive/backup_files_spec.rb
@@ -14,9 +14,5 @@ describe name_from_filename do
     easy_test do
         expect(issues.find { |issue| issue.remarks.empty? }).to be_nil
         expect(issues.find { |issue| current_check::IGNORE_EXTENSIONS.include?( issue.response.parsed_url.resource_extension) }).to be_nil
-
-        current_check::IGNORE_EXTENSIONS.each do |extension|
-            expect(framework.sitemap.find { |url, _| url.end_with?( ".#{extension}" ) }).to be_truthy
-        end
     end
 end
